7a55147bbbc2c98ea084ca72290ee82afec28945
Tyler Zhu <tyler24.nl@gmail.com>
2017-02-25 05:38:22 -0800

    add creating Figure dir code to discreteProbDistFig.py

--- figureCode/discreteProbDistFig.py
+++ figureCode/discreteProbDistFig.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 
 # Bar graphs showing a uniform discrete distribution and another with full prob on one value.
+FIGURE_DIR = 'figures'
 
 X = np.arange(1, 5)
 UniProbs = np.repeat(1.0/len(X),len(X))
@@ -13,8 +14,10 @@ def MakeG(Probs,SaveN):
     plt.xlim([min(X) - .5, max(X) + .5])
     plt.xticks(X)
     plt.yticks(np.linspace(0, 1, 5))
-    #plt.savefig(SaveN)
-    plt.savefig(os.path.join('figures', SaveN))
+    if not os.path.exists(FIGURE_DIR):
+        os.mkdir(FIGURE_DIR)
+    figure_path = os.path.join(FIGURE_DIR, SaveN)
+    plt.savefig(figure_path)
 
 MakeG(UniProbs, "ProbsDiscreteUniform")
 MakeG([1, 0, 0, 0], "ProbsDiscreteDelta")
