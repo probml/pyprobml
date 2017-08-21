62ea2e3ee8563fae1e5e3f3047a99fbc2d062d8c
Kevin Murphy <murphyk@gmail.com>
2017-08-21 06:59:35 -0700

    Plot surfaces as well as contours. Switched to python PEP style.

--- examples/gaussPlot2Ddemo.py
+++ examples/gaussPlot2Ddemo.py
@@ -22,23 +22,29 @@ Gpdf = np.vectorize(Gpdf, excluded=['G'])
 points = np.linspace(-5, 5, 100)
 X, Y = np.meshgrid(points, points)
 
-def MakeGraph(G):
+def make_contour_plot(G):
     Z = Gpdf(X, Y, G)
     fig, ax = plt.subplots()
     ax.contour(X, Y, Z)
+    plt.axis('equal')
     plt.title(G)
     plt.draw()
-    plt.savefig('figures/Gaussian2D' + G)
+    plt.show()
+    plt.savefig('figures/Gaussian2D' + G+ 'Contour')
 
+
+def make_surface_plot(G):
+    Z = Gpdf(X, Y, G)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=2, cstride=2, color='white', edgecolor="black")
+    #ax.axis('equal')
+    #ax.title(G)f
+    plt.draw()
+    plt.show()
+    plt.savefig('figures/Gaussian2D' + G + 'Surface')
+    
 for g in Gs:
-    MakeGraph(g)
-
-# Surface plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-Z = Gpdf(X, Y, "spherical")
-
-ax.plot_surface(X, Y, Z, rstride=2, cstride=2, color='white', edgecolor="black")
-plt.savefig('figures/Gaussian2DSurface')
-
-plt.show()
+    make_contour_plot(g)
+    make_surface_plot(g)
+    
