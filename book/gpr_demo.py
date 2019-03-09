f00152ce42cc657dcb09664e9a26e1c750ccc4a5
Kevin Murphy <murphyk@gmail.com>
2019-03-09 07:05:19 +0000

    custom kernel inside BO

---
+++ book/gpr_demo.py
@@ -0,0 +1,282 @@
+# Modified from Martin Krasser's code
+# https://github.com/krasserm/bayesian-machine-learning/blob/master/gaussian_processes.ipynb
+# Apache 2.0 license
+
+# We assume the mean function is 0
+
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+from utils import save_fig
+
+np.random.seed(0)
+
+def plot_gp(mu, cov, X, X_train=None, Y_train=None, samples=[]):
+    X = X.ravel()
+    mu = mu.ravel()
+    uncertainty = 1.96 * np.sqrt(np.diag(cov))
+    plt.fill_between(X, mu + uncertainty, mu - uncertainty, alpha=0.1)
+    plt.plot(X, mu, label='Mean')
+    for i, sample in enumerate(samples):
+        plt.plot(X, sample, lw=1, ls='--', label=f'Sample {i+1}')
+    if X_train is not None:
+        plt.plot(X_train, Y_train, 'rx')
+    plt.legend()
+
+def plot_gp_2D(gx, gy, mu, X_train, Y_train, title, i):
+    ax = plt.gcf().add_subplot(1, 2, i, projection='3d')
+    ax.plot_surface(gx, gy, mu.reshape(gx.shape), cmap=cm.coolwarm, linewidth=0, alpha=0.2, antialiased=False)
+    ax.scatter(X_train[:,0], X_train[:,1], Y_train, c=Y_train, cmap=cm.coolwarm)
+    ax.set_title(title)
+    
+def rbf_kernel(X1, X2, l=1.0, sigma_f=1.0):
+    '''
+    Isotropic squared exponential kernel. Computes 
+    a covariance matrix from points in X1 and X2.
+    
+    Args:
+        X1: Array of m points (m x d).
+        X2: Array of n points (n x d).
+
+    Returns:
+        Covariance matrix (m x n).
+    '''
+    sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+    return sigma_f**2 * np.exp(-0.5 / l**2 * sqdist)
+
+def kernel(X1, X2, *args, **kwargs):
+  return rbf_kernel(X1, X2, *args, **kwargs)
+
+# Finite number of points
+X = np.arange(-5, 5, 0.2).reshape(-1, 1)
+
+# Mean and covariance of the prior
+mu = np.zeros(X.shape)
+cov = kernel(X, X)
+
+# Draw three samples from the prior
+samples = np.random.multivariate_normal(mu.ravel(), cov, 3)
+
+# Plot GP mean, confidence interval and samples 
+plot_gp(mu, cov, X, samples=samples)
+save_fig('gp-prior-samples.pdf')
+plt.show()
+
+
+def posterior_predictive(X_s, X_train, Y_train, l=1.0, sigma_f=1.0, sigma_y=1e-8):
+    '''
+    Computes the suffifient statistics of the GP posterior predictive distribution 
+    from m training data X_train and Y_train and n new inputs X_s.
+    
+    Args:
+        X_s: New input locations (n x d).
+        X_train: Training locations (m x d).
+        Y_train: Training targets (m x 1).
+        l: Kernel length parameter.
+        sigma_f: Kernel vertical variation parameter.
+        sigma_y: Noise parameter.
+    
+    Returns:
+        Posterior mean vector (n x d) and covariance matrix (n x n).
+    '''
+    K = kernel(X_train, X_train, l, sigma_f) + sigma_y**2 * np.eye(len(X_train))
+    K_s = kernel(X_train, X_s, l, sigma_f)
+    K_ss = kernel(X_s, X_s, l, sigma_f) + 1e-8 * np.eye(len(X_s))
+    K_inv = inv(K)
+    mu_s = K_s.T.dot(K_inv).dot(Y_train)
+    cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)    
+    return mu_s, cov_s
+  
+# Noise free training data
+X_train = np.array([-4, -3, -2, -1, 1]).reshape(-1, 1)
+Y_train = np.sin(X_train)
+
+# Compute mean and covariance of the posterior predictive distribution
+mu_s, cov_s = posterior_predictive(X, X_train, Y_train)
+
+samples = np.random.multivariate_normal(mu_s.ravel(), cov_s, 3)
+plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train, samples=samples)
+save_fig('gp-post-noise-free-samples.pdf')
+plt.show()
+
+
+noise = 0.4
+# Noisy training data
+X_train = np.arange(-3, 4, 1).reshape(-1, 1)
+Y_train = np.sin(X_train) + noise * np.random.randn(*X_train.shape)
+# Compute mean and covariance of the posterior predictive distribution
+mu_s, cov_s = posterior_predictive(X, X_train, Y_train, sigma_y=noise)
+samples = np.random.multivariate_normal(mu_s.ravel(), cov_s, 3)
+plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train, samples=samples)
+save_fig('gp-post-samples.pdf')
+plt.show()
+
+
+
+
+params = [
+    (0.3, 1.0, 0.3),
+    (1.0, 1.0, 0.3),
+    (3.0, 1.0, 0.3),
+    (1.0, 0.3, 0.3),
+    (1.0, 1.0, 0.3),
+    (1.0, 3.0, 0.3),
+    (1.0, 0.3, 0.3),
+    (1.0, 0.3, 1.0),
+    (1.0, 0.3, 3.0)
+]
+
+plt.figure(figsize=(12, 12))
+
+for i, (l, sigma_f, sigma_y) in enumerate(params):
+    mu_s, cov_s = posterior_predictive(X, X_train, Y_train, l=l, 
+                                       sigma_f=sigma_f, 
+                                       sigma_y=sigma_y)
+    plt.subplot(3, 3, i + 1)
+    plt.subplots_adjust(top=2)
+    plt.title(f'l = {l}, sigma_f = {sigma_f}, sigma_y = {sigma_y}')
+    plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train)
+save_fig('gp-hparams.pdf')
+plt.show()
+  
+
+
+
+from numpy.linalg import cholesky
+from scipy.optimize import minimize
+
+def nll_fn(X_train, Y_train, noise):
+    '''
+    Returns a function that computes the negative log-likelihood
+    for training data X_train and Y_train and given noise level.
+    
+    Args:
+        X_train: training locations (m x d).
+        Y_train: training targets (m x 1).
+        noise: known noise level of Y_train.
+        
+    Returns:
+        Minimization objective.
+    '''
+    def step(theta):
+        K = kernel(X_train, X_train, l=theta[0], sigma_f=theta[1]) + \
+            noise**2 * np.eye(len(X_train))
+        # Compute determinant via Cholesky decomposition
+        return np.sum(np.log(np.diagonal(cholesky(K)))) + \
+               0.5 * Y_train.T.dot(inv(K).dot(Y_train)) + \
+               0.5 * len(X_train) * np.log(2*np.pi)
+    return step
+
+# Minimize the negative log-likelihood w.r.t. parameters l and sigma_f.
+# We should actually run the minimization several times with different
+# initializations to avoid local minima but this is skipped here for
+# simplicity.
+res = minimize(nll_fn(X_train, Y_train, noise), [1, 1], 
+               bounds=((1e-5, None), (1e-5, None)),
+               method='L-BFGS-B')
+
+# Store the optimization results in global variables so that we can
+# compare it later with the results from other implementations.
+l_opt, sigma_f_opt = res.x
+l_opt, sigma_f_opt
+
+# Compute the prosterior predictive statistics with optimized kernel parameters and plot the results
+mu_s, cov_s = posterior_predictive(X, X_train, Y_train, l=l_opt, sigma_f=sigma_f_opt, sigma_y=noise)
+plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train)
+save_fig('gp-fitted.pdf')
+plt.show()
+
+
+#################
+
+
+noise_2D = 0.1
+rx, ry = np.arange(-5, 5, 0.3), np.arange(-5, 5, 0.3)
+gx, gy = np.meshgrid(rx, rx)
+
+X_2D = np.c_[gx.ravel(), gy.ravel()]
+
+X_2D_train = np.random.uniform(-4, 4, (100, 2))
+Y_2D_train = np.sin(0.5 * np.linalg.norm(X_2D_train, axis=1)) + \
+             noise_2D * np.random.randn(len(X_2D_train))
+
+plt.figure(figsize=(14,7))
+mu_s, _ = posterior_predictive(X_2D, X_2D_train, Y_2D_train, sigma_y=noise_2D)
+plot_gp_2D(gx, gy, mu_s, X_2D_train, Y_2D_train, 
+           f'Before parameter optimization: l={1.00} sigma_f={1.00}', 1)
+save_fig('gp-2d-unfitted.pdf')
+plt.show()
+
+res = minimize(nll_fn(X_2D_train, Y_2D_train, noise_2D), [1, 1], 
+               bounds=((1e-5, None), (1e-5, None)),
+               method='L-BFGS-B')
+
+plt.figure(figsize=(14,7))
+mu_s, _ = posterior_predictive(X_2D, X_2D_train, Y_2D_train, *res.x, sigma_y=noise_2D)
+plot_gp_2D(gx, gy, mu_s, X_2D_train, Y_2D_train,
+           f'After parameter optimization: l={res.x[0]:.2f} sigma_f={res.x[1]:.2f}', 2)
+save_fig('gp-2d-fitted.pdf')
+plt.show()
+
+#################
+
+# SKLearn equivalent method
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+
+rbf = ConstantKernel(1.0) * RBF(length_scale=1.0)
+gpr = GaussianProcessRegressor(kernel=rbf, alpha=noise**2)
+
+# Reuse training data from previous 1D example
+gpr.fit(X_train, Y_train)
+
+# Compute posterior predictive mean and covariance
+mu_s, cov_s = gpr.predict(X, return_cov=True)
+
+# Obtain optimized kernel parameters
+l = gpr.kernel_.k2.get_params()['length_scale']
+sigma_f = np.sqrt(gpr.kernel_.k1.get_params()['constant_value'])
+
+# Compare with previous results
+assert(np.isclose(l_opt, l))
+assert(np.isclose(sigma_f_opt, sigma_f))
+
+# Plot the results
+plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train)
+
+
+############
+# https://sheffieldml.github.io/GPy/
+# See also https://gpytorch.ai/
+import sys
+sys.path.append("/home/kpmurphy/github/GPy")
+
+import GPy
+
+rbf = GPy.kern.RBF(input_dim=1, variance=1.0, lengthscale=1.0)
+gpr = GPy.models.GPRegression(X_train, Y_train, rbf)
+
+# Fix the noise variance to known value 
+gpr.Gaussian_noise.variance = noise**2
+gpr.Gaussian_noise.variance.fix()
+
+# Run optimization
+gpr.optimize();
+
+# Display optimized parameter values
+#display(gpr)
+
+# Obtain optimized kernel parameters
+l = gpr.rbf.lengthscale.values[0]
+sigma_f = np.sqrt(gpr.rbf.variance.values[0])
+
+# Compare with previous results
+assert(np.isclose(l_opt, l))
+assert(np.isclose(sigma_f_opt, sigma_f))
+
+# Plot the results with the built-in plot function
+gpr.plot();
--- scratch/boss.py
+++ scratch/boss.py
@@ -70,14 +70,24 @@ def gen_all_dna(seq_len):
   return np.stack(S)
 
 
+plt.figure()
+plt.plot(yall)
+
 # Extract training set based on "medium performing" strings
+# These could be unlabeled (if we use an RNN feature extractor)
 bins = pd.qcut(yall, 100, labels=False, duplicates='drop')
-middle_bins = np.where(np.logical_or(bins>=25, bins<=75))
+middle_bins = np.where(np.logical_and(bins>=25, bins<=75))
 Xtrain = Xall[middle_bins]
 ytrain = yall[middle_bins]
 ntrain = np.shape(Xtrain)[0]
 print("Training set has {} examples from {}".format(ntrain, nseq))
 
+# Pick a small labeled subset for training the GP
+perm = np.random.permutation(ntrain)
+ninit = 10
+perm = perm[:ninit]
+Xinit = Xtrain[perm]
+yinit = ytrain[perm]
 
 embed_dim = 5 # D 
 nhidden = 10
@@ -155,13 +165,17 @@ class EnumerativeSolver:
     self.seq_len = seq_len
     Xall = gen_all_dna(seq_len) # could use iterator
     nseq = np.shape(Xall)[0]
+    # We enumerate them in a random order, in case the optimal sequence
+    # is near the start.
     perm = np.random.permutation(nseq)
     self.Xall = Xall[perm]
     self.ndx = 0
     self.current_best_seq = None
     self.current_best_val = np.inf
+    self.nqueries = 0
   
   def propose(self):
+    self.nqueries += 1
     x = self.Xall[self.ndx]
     if self.ndx == nseq:
       self.ndx = 0
@@ -178,15 +192,109 @@ class EnumerativeSolver:
     return (self.current_best_seq, self.current_best_val)
 
 
-class GPSolver:
-  def __init__(self, seq_len, ninit):
+
+nsteps = 100
+solver = EnumerativeSolver(seq_len)
+history = []
+for t in range(nsteps):
+  x = solver.propose()
+  y = oracle(x)
+  solver.update(x, y)
+  xbest, ybest = solver.current_best()
+  history.append(ybest)
+  
+plt.figure()
+plt.plot(range(nsteps), history)
+plt.title('Enumerative solver')
+
+
+
+
+
+from skopt.learning import GaussianProcessRegressor
+from skopt.learning.gaussian_process.kernels import ConstantKernel, Matern, RBF
+
+class StringEmbedKernel(Matern):
+  def __init__(self, length_scale=1.0, length_scale_bounds=(1e-5, 1e5),
+                 nu=1.5, seq_len=None, embedder=None):
+        super().__init__(length_scale, length_scale_bounds)
+        self.seq_len = seq_len
+        self.embedder = embedder
+ 
+  def __call__(self, X, Y=None, eval_gradient=False):
+    print("seq len = {}".format(self.seq_len))
+    X = self.embedder(X)
+    if Y is not None:
+      Y = self.embedder(Y)
+    return super().__call__(X, Y=Y, eval_gradient=eval_gradient)
+
+"""
+https://github.com/scikit-learn/scikit-learn/blob/7b136e9/sklearn/gaussian_process/kernels.py#L1287
+The parameter nu controlling the smoothness of the learned function.
+        The smaller nu, the less smooth the approximated function is.
+        For nu=inf, the kernel becomes equivalent to the RBF kernel and for
+        nu=0.5 to the absolute exponential kernel. Important intermediate
+        values are nu=1.5 (once differentiable functions) and nu=2.5
+        (twice differentiable functions). Note that values of nu not in
+        [0.5, 1.5, 2.5, inf] incur a considerably higher computational cost
+        (appr. 10 times higher) since they require to evaluate the modified
+        Bessel function. Furthermore, in contrast to l, nu is kept fixed to
+        its initial value and not optimized.
+"""
+
+# https://github.com/krasserm/bayesian-machine-learning/blob/master/bayesian_optimization.ipynb
+from scipy.stats import norm
+def expected_improvement(X, X_sample, Y_sample, gpr, xi=0.01):
+    '''
+    Computes the EI at points X based on existing samples X_sample
+    and Y_sample using a Gaussian process surrogate model.
+    
+    Args:
+        X: Points at which EI shall be computed (m x d).
+        X_sample: Sample locations (n x d).
+        Y_sample: Sample values (n x 1).
+        gpr: A GaussianProcessRegressor fitted to samples.
+        xi: Exploitation-exploration trade-off parameter.
+    
+    Returns:
+        Expected improvements at points X.
+    '''
+    mu, sigma = gpr.predict(X, return_std=True)
+    mu_sample = gpr.predict(X_sample)
+
+    #sigma = sigma.reshape(-1, X_sample.shape[1])
+    
+    # Needed for noise-based model,
+    # otherwise use np.max(Y_sample).
+    # See also section 2.4 in [...]
+    mu_sample_opt = np.max(mu_sample)
+
+    with np.errstate(divide='warn'):
+        imp = mu - mu_sample_opt - xi
+        Z = imp / sigma
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+
+    return ei
+  
+class GPMaximizer:
+  def __init__(self, seq_len, embedder, Xinit, yinit, noise_std=None):
     self.seq_len = seq_len
-    self.ninit = ninit
+    self.noise_std = noise_std
     self.current_best_seq = None
     self.current_best_val = np.inf
+    self.X_sample = []
+    self.y_sample = []
     self.nqueries = 0
+    m52 = ConstantKernel(1.0) * StringEmbedKernel(seq_len=seq_len, embedder=embedder)
+    if noise_std is None:
+      noise_std = np.std(yinit)
+    gpr = GaussianProcessRegressor(kernel=m52, alpha=noise_std**2)
+    gpr.fit(Xinit, yinit)
+    self.gpr = gpr
   
   def propose(self):
+    self.nqueries += 1
     return gen_rnd_dna(self.seq_len)
  
   def update(self, x, y):
@@ -197,12 +305,19 @@ class GPSolver:
   def current_best(self):
     return (self.current_best_seq, self.current_best_val)
   
+
+def embed(x):
+  return embedder.predict(x)
+
+
+    
 nsteps = 100
-solver = EnumerativeSolver(seq_len)
+solver = GPMaximizer(seq_len, embed, Xinit, yinit)
 history = []
 for t in range(nsteps):
   x = solver.propose()
   y = oracle(x)
+  print("queried {}, observed {}".format(x, y))
   solver.update(x, y)
   xbest, ybest = solver.current_best()
   history.append(ybest)
