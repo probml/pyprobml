329c407dcdc1b40906bd16c160be3e830766beef
Kevin Murphy <murphyk@gmail.com>
2019-03-12 09:54:13 -0700

    tweaks

--- book/iris_linreg.py
+++ book/iris_linreg.py
@@ -1,23 +1,13 @@
+# Linear regression on iris dataset
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns;
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets
-import os
+from utils import save_fig
 
-# Where to save the figures
-IMAGES_PATH = "../figures"
-os.makedirs(IMAGES_PATH, exist_ok=True)
 
-def save_fig(fig_id, tight_layout=True, fig_extension="pdf", resolution=300):
-    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format=fig_extension, dpi=resolution)
-    
-# import some data to play with
 iris = datasets.load_iris()
 xidx = 2
 
