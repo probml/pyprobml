9c0dcd8ee1e3dfb81433c44bd7323757b0ad8582
Kevin Murphy <murphyk@gmail.com>
2019-03-21 23:21:44 -0700

    broken code from duane

--- figureCode/linregPolyVsN.py
+++ figureCode/linregPolyVsN.py
@@ -1,4 +1,4 @@
-from polyDataMake import polyDataMake
+
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import MinMaxScaler 
 import matplotlib.pyplot as plt
@@ -12,6 +12,44 @@ degrees = [1, 2, 10, 50] #The degrees of our design matrices
 def ExpandtoDeg(x,deg):
     return np.array([x**i for i in range(deg+1)]).transpose().reshape(-1,deg+1)
 
+
+def poly_data_make(sampling="sparse", deg=3, n=21):
+    """
+    Create an artificial dataset
+    """
+    np.random.seed(0)
+
+    if sampling == "irregular":
+        xtrain = np.concatenate(
+            (np.arange(-1, -0.5, 0.1), np.arange(3, 3.5, 0.1)))
+    elif sampling == "sparse":
+        xtrain = np.array([-3, -2, 0, 2, 3])
+    elif sampling == "dense":
+        xtrain = np.arange(-5, 5, 0.6)
+    elif sampling == "thibaux":
+        xtrain = np.linspace(0, 20, n)
+        xtest = np.arange(0, 20, 0.1)
+        sigma2 = 4
+        w = np.array([-1.5, 1/9.])
+        fun = lambda x: w[0]*x + w[1]*np.square(x)
+
+    if sampling != "thibaux":
+        assert deg < 4, "bad degree, dude %d" % deg
+        xtest = np.arange(-7, 7, 0.1)
+        if deg == 2:
+            fun = lambda x: (10 + x + np.square(x))
+        else:
+            fun = lambda x: (10 + x + np.power(x, 3))
+        sigma2 = np.square(5)
+
+    ytrain = fun(xtrain) + np.random.normal(0, 1, xtrain.shape) * \
+        np.sqrt(sigma2)
+    ytestNoisefree = fun(xtest)
+    ytestNoisy = ytestNoisefree + np.random.normal(0, 1, xtest.shape) * \
+        np.sqrt(sigma2)
+
+    return xtrain, ytrain, xtest, ytestNoisefree, ytestNoisy, sigma2
+
 for ModDeg in degrees:
     
     ns = np.round(np.linspace(10, 200, 10))
@@ -20,7 +58,7 @@ for ModDeg in degrees:
     errtrain = []
     for n in ns:
         #Forming data
-        xtrain, ytrain, xtest, _, ytest, _ = polyDataMake(n, sampling='thibaux',deg=TrueDeg)
+        xtrain, ytrain, xtest, _, ytest, _ = poly_data_make(n=n, sampling='thibaux',deg=TrueDeg)
         
         #Rescaling data
         scaler = MinMaxScaler(feature_range=(-1, 1))
