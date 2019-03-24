74f86d3149c1454ef34fb0dde95f07c027cdd556
Kevin Murphy <murphyk@gmail.com>
2019-03-23 18:27:40 -0700

    broken

--- book/beta_binom_post_pred_plot.py
+++ book/beta_binom_post_pred_plot.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
-#
+
 # Plots the posterior and plugin predictives for the Beta-Binomial distribution.
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+figdir = os.path.join(os.environ["PYPROBML"], "figures")
+def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
 
 from scipy.misc import comb
 from scipy.special import beta
 from scipy.stats import binom
-import matplotlib.pyplot as pl
-import numpy as np
+
 
 N = 10
 # Hyperparameters
@@ -23,13 +28,13 @@ distribution = []
 for k in range(N+1):
   distribution.append(comb(N,k) * beta(k+post_a, N-k+post_b) / beta(post_a, post_b))
 
-fig,ax = pl.subplots()
+fig,ax = plt.subplots()
 rects = ax.bar(ind, distribution, align='center')
 ax.set_title('posterior predictive')
 ax.set_xticks(list(range(N+1)))
 ax.set_xticklabels(list(range(N+1)))
-pl.savefig('BBposteriorpred.png')
-pl.show()
+save_fig('BBpostpred.pdf')
+plt.show()
 
 mu = (post_a - 1) / float(post_a + post_b - 2)
 distribution = []
@@ -37,13 +42,13 @@ rv = binom(N, mu)
 for k in range(N+1):
   distribution.append(rv.pmf(k))
 
-fig,ax = pl.subplots()
+fig,ax = plt.subplots()
 rects = ax.bar(ind, distribution, align='center')
 ax.set_title('incremental predictive')
 ax.set_xticks(list(range(N+1)))
 ax.set_xticklabels(list(range(N+1)))
 
-pl.savefig('BBpluginpred.png')
-pl.show()
+save_fig('BBpluginpred.pdf')
+plt.show()
 
 
