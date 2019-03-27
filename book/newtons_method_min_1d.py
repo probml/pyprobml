0d4ad6f9a17d92572e61e1e343a04edd6ce146de
andrewnc <andrewcarr06@gmail.com>
2019-03-27 11:56:15 -0600

    plot min_1d figure, didn't plot labels for data 1 - 4 because I think it is much more clean this way

---
+++ book/newtons_method_min_1d.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def save_fig(fname):
+    figdir = os.path.join(os.environ["PYPROBML"], "figures")
+    plt.tight_layout()    
+    fullname = os.path.join(figdir, fname)
+    print('saving to {}'.format(fullname))
+    plt.savefig(fullname)
+
+
+xmin = 0.1
+xmax = 12
+ymin= -5
+ymax = 4
+domain = np.linspace(xmin, xmax, 1191) # num=1191 assumes a step size of 0.01 for this domain
+f = lambda x: np.log(x) - 2
+
+x_k = 2
+m = 1/x_k
+b = f(x_k) - m*x_k
+tl = lambda x: m*x + b
+
+plt.plot((0.1, 12), (0,0), '-k', linewidth=2, zorder=1)
+plt.plot(domain, f(domain), '-r', linewidth=3, label=r"$g(x)$", zorder=2)
+plt.plot(domain, tl(domain), '--b', linewidth=2.5, label=r"$g_{lin}(x)$", zorder=3)
+
+plt.scatter(x_k, f(x_k), marker='.', c='black', s=180, zorder=4)
+plt.scatter(-b/m, 0, marker='.', c='black', s=180, zorder=4)
+plt.plot((x_k, x_k), (ymin, f(x_k)), ":k")
+plt.plot((-b/m, -b/m), (ymin, 0), ":k")
+
+plt.xlim((xmin, xmax))
+plt.ylim((ymin, ymax))
+
+frame1 = plt.gca()
+frame1.axes.xaxis.set_ticklabels([])
+frame1.axes.yaxis.set_ticklabels([])
+
+plt.xticks([x_k, -b/m], [r'$x_{k}$', r'$x_{k} + d_{k}$'])
+
+
+plt.legend()
+save_fig("newtonsMethodMin1d.pdf")
+plt.show()
+
