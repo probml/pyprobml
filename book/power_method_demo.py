b12714844d83260b782bde4a51cac03e722827fe
Kevin Murphy <murphyk@gmail.com>
2019-03-29 20:42:28 -0700

    linalg demos

---
+++ book/power_method_demo.py
@@ -0,0 +1,33 @@
+# Illustrate the powr method for computing largest eigenvector
+
+import numpy as np
+from numpy.linalg import norm
+
+np.random.seed(0)
+
+def power_method(A, max_iter=100, tol=1e-5):
+    n = np.shape(A)[0]
+    u = np.random.rand(n)
+    converged = False
+    iter = 0
+    while (not converged) and (iter < max_iter):
+        old_u = u
+        u = np.dot(A, u)
+        u = u / norm(u)
+        lam = np.dot(u, np.dot(A, u))
+        converged = (norm(u - old_u) < tol)
+        iter += 1
+    return lam, u
+
+X = np.random.randn(10, 5)
+A = np.dot(X.T, X)
+lam, u = power_method(A)
+
+evals, evecs = np.linalg.eig(A)
+idx = np.argsort(np.abs(evals))[::-1] # largest first
+evals = evals[idx]
+evecs = evecs[:,idx]
+
+tol = 1e-3
+assert np.allclose(evecs[:,0], u, tol)
+assert np.allclose(evals[0], lam, tol)
--- book/rotation_demo.py
+++ book/rotation_demo.py
@@ -1,31 +1,31 @@
 import numpy as np
 
 a = (45/180) * np.pi
-R = np.array([[np.cos(a), -np.sin(a), 0],
-     [np.sin(a), np.cos(a), 0],
-     [0, 0, 1]])
+R = np.array(
+        [[np.cos(a), -np.sin(a), 0],
+          [np.sin(a), np.cos(a), 0],
+          [0, 0, 1]])
 
 print("\nR = ")
 print(R)
 
-A = R.dot(np.diag([1,2,3])).dot(R.T)
-
+S = np.diag([1.0, 2.0, 3.0])
+A = np.dot(np.dot(R, S), R.T)
 print("\nA = ")
 print(A)
 
-vals, vecs = np.linalg.eig(A)
+evals, evecs = np.linalg.eig(A)
+idx = np.argsort(np.abs(evals)) # smallest first
+U = evecs[:, idx] # sort columns
+D = np.diag(evals[idx])
 
-print("\nU = ")
-print(vecs)
-
-print("\nD = ")
-print(np.diag(sorted(vals)))
-
+assert np.allclose(np.abs(R), np.abs(U))
+assert np.allclose(D, S)
+assert np.allclose(A, np.dot(U, np.dot(D, U.T)))
 
 a = -a
 RR = np.array([[np.cos(a), -np.sin(a), 0],
      [np.sin(a), np.cos(a), 0],
      [0, 0, 1]])
 
-print()
-print(np.allclose(R.T, RR))
+assert np.allclose(R.T, RR)
