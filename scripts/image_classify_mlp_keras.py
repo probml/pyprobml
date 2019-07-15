7c9ddc1e81c02a3130ec12bcd15639c63490ea6c
Kevin Murphy <murphyk@gmail.com>
2019-07-15 16:03:36 -0700

    boston trip stuff

---
+++ scripts/image_classify_mlp_keras.py
@@ -0,0 +1,148 @@
+# Based on
+# https://github.com/tensorflow/docs/blob/master/site/en/tutorials/keras/basic_classification.ipynb
+# (MIT License)
+
+from __future__ import absolute_import, division, print_function
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+figdir = "../figures"
+def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
+
+
+import tensorflow as tf
+from tensorflow import keras
+from time import time
+
+print(tf.__version__)
+np.random.seed(0)
+
+data = keras.datasets.cifar10
+#data = keras.datasets.mnist
+#data = keras.datasets.fashion_mnist
+
+
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+print(np.shape(train_images))
+print(np.shape(test_images))
+
+# For MNIST:
+#(60000, 28, 28)
+#(10000, 28, 28)
+
+# For CIFAR:
+# (50000, 32, 32, 3)
+# (10000, 32, 32, 3)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+save_fig("fashion-mnist-data.pdf")
+plt.show()
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+# We just train for 2 epochs because (1) it is faster, and (2)
+# it produces more errors, which makes for a more interesting plot :)
+time_start = time()
+model.fit(train_images, train_labels, epochs=2)
+print('time spent training {:0.3f}'.format(time() - time_start))
+
+predictions = model.predict(test_images)
+print(np.shape(predictions))
+
+# Overall accuracy
+train_loss, train_acc = model.evaluate(train_images, train_labels)
+print('Train accuracy:', train_acc)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+"""
+60000/60000 [==============================] - 4s 65us/sample - loss: 0.3328 - acc: 0.8784
+Train accuracy: 0.87841666
+10000/10000 [==============================] - 1s 140us/sample - loss: 0.3851 - acc: 0.8615
+Test accuracy: 0.8615
+"""
+
+# To apply prediction to a single image, we need to reshape to an (N,D,D) tensor
+# where N=1
+img = test_images[0]
+img = (np.expand_dims(img,0))
+print(img.shape)
+predictions_single = model.predict(img)
+print(predictions_single.shape)
+"""
+(1, 28, 28)
+(1, 10)
+"""
+
+def plot_image_and_label(predictions_array, true_label, img):
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  plt.imshow(img, cmap=plt.cm.binary)
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+    plt.xlabel("{} {:2.0f}%".format(class_names[predicted_label],
+                                100*np.max(predictions_array)),
+                                color=color)
+  else:
+    color = 'red'
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_label_dist(predictions_array, true_label):
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
+  
+# Plot the first 15 test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image_and_label(predictions[i], test_labels[i], test_images[i])
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_label_dist(predictions[i], test_labels[i])
+save_fig("fashion-mnist-predictions.pdf")
+plt.show()
+
+
+
+
+
---
+++ scripts/proxlinear.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+figdir = "../figures"
+def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
+
+xs = np.arange(-2, 2, step=0.01)
+y1 = xs ** 2
+y2 = y1 - 1
+y3 = np.abs(y2)
+y4 = y3 + y1
+plt.figure()
+plt.plot(xs, y1, 'r-', label='x^2')
+plt.plot(xs, y2, 'g:', label='x^2 - 1')
+plt.plot(xs, y3, 'b-', label='|x^2 - 1|')
+plt.plot(xs, y4, 'k-', label='|x^2 - 1| + x^2')
+plt.legend()
+save_fig('weaklyConvex.png')
+plt.show()
+
+def f(x):
+    return np.abs(x**2 - 1)
+
+def grad(xp):
+    return 2*xp
+
+def lin(x, xp):
+    g = grad(xp)
+    return np.abs(f(xp) - (x-xp)*g)
+
+
+ys = [f(x) for x in xs]
+x1 = 0.5
+x2 = -1.0
+zs1 = [lin(x, x1) for x in xs]
+zs2 = [lin(x, x2) for x in xs]
+plt.figure()
+plt.plot(xs, ys, 'b', label='f')
+plt.plot(xs, zs1, 'r--', label='$f_{x_1}$')
+plt.plot(xs, zs2, 'r-', label='$f_{x_2}$')
+plt.axhline(y=0)
+plt.ylim([-0.5, 3])
+plt.annotate('$(x_1, f(x_1))$', xy=(0.5, 0.75))
+plt.annotate('$(x_2, f(x_2))$', xy=(-1, -0.2))
+plt.legend(loc='upper right', fontsize=14)
+save_fig('proxLinear.png')
+plt.show()
