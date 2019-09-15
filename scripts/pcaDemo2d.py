2a4bab397d0c69e14d309cf1d0511fedd861620c
Duane Rich <drich@lyft.com>
2019-09-15 12:02:11 -0700

    add pcaDemo2d for fig 2.70

---
+++ scripts/pcaDemo2d.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import os
+
+np.random.seed(42)
+
+# Generate Data
+n = 9
+var = 3
+corr = .5
+
+cov_mat = [[var, corr * var], [corr * var, var]]
+X = np.random.multivariate_normal([0, 0], cov_mat, n)
+
+# Project Data onto PCA line
+subspace_dim = 1
+pca = PCA(subspace_dim)
+X_reconstr = pca.inverse_transform(pca.fit_transform(X))
+
+# Create figure and save.
+fig, ax = plt.subplots(figsize=(5, 5))
+
+# Plot raw data
+ax.scatter(X[:, 0], X[:, 1], marker='o', facecolor='none', edgecolor='red')
+X_mean = np.mean(X, axis=0)
+ax.scatter(X_mean[0], X_mean[1], facecolor='red')
+
+# Plot PCA line
+low_point = X_mean - 10 * pca.components_.reshape(-1)
+high_point = X_mean + 10 * pca.components_.reshape(-1)
+ax.plot([low_point[0], high_point[0]], [low_point[1], high_point[1]], color='magenta')
+ax.set_ylim(-5, 5)
+ax.set_xlim(-5, 5)
+
+# Plot projected points
+ax.scatter(X_reconstr[:, 0], X_reconstr[:, 1], marker='o', facecolor='none', edgecolor='teal')
+
+# Plot projection lines
+for (xi1, xi2), (xi1_rec, xi2_rec) in zip(X, X_reconstr):
+    ax.plot([xi1, xi1_rec], [xi2, xi2_rec], color='blue')
+
+# Save figure
+figdir = "../figures"
+#
+def save_fig(fname):
+    plt.savefig(os.path.join(figdir, fname))
+
+save_fig("pcaDemo2dProjection")
+
+plt.draw()
