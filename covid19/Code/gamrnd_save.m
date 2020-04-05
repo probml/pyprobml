fdfa8d1b9c51486c71a46c16421ca5453d805afc
Kevin Murphy <murphyk@gmail.com>
2020-04-05 09:26:17 -0700

    csv format is more portable than .mat :)

--- covid19/Code/gamrnd_save.m
+++ covid19/Code/gamrnd_save.m
@@ -4,3 +4,4 @@ a=1.85;%shape parameter of gamma distribution
 b=Td/a;%scale parameter of gamma distribution
 gam_rnds=ceil(gamrnd(a,b,1e4,1));%pre-generate gamma random numbers
 save('../Data/gamrnd_seed42', 'gam_rnds');
+csvwrite('../Data/gamrnd_seed42.csv', gam_rnds);
