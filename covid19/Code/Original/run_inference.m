02145585b6f145d12a3b7654113d474326fa7e92
Kevin Murphy <murphyk@gmail.com>
2020-04-09 16:36:44 -0700

    save experiemtnal results

--- covid19/Code/Original/run_inference.m
+++ covid19/Code/Original/run_inference.m
@@ -1,9 +1,9 @@
 load('../../Data/Mobility.mat')
 load('../../Data/pop.mat')
 load('../../Data/incidence.mat')
-num_iter = 5;
-num_ens = 100;
-seeds = 1:5;
+num_iter = 2;
+num_ens = 10;
+seeds = 1:2;
 nseeds = length(seeds);
 param_dist = zeros(6, nseeds);
 tic
@@ -18,6 +18,8 @@ for seedi = 1:nseeds
     param_dist(:,seedi) = theta(:,end);
 end
 toc  % 10 minutes for 5 seeds, 5 iters, 100 samples
+fname = sprintf('param_dist_%dseeds_%dsamples_%diter', nseeds, num_ens, num_iter);
+save(fname, 'param_dist')
 
 names = {'beta', 'mu', 'theta', 'Z', 'alpha', 'D'};
 figure; 
@@ -32,7 +34,6 @@ end
 suptitle(sprintf('Original MLEs over %d seeds, %d samples, %d iter',  nseeds, num_ens, num_iter));
 fname = sprintf('param_boxplot_%dseeds_%dsamples_%diter', nseeds, num_ens, num_iter);
 print(gcf, fname, '-dpng')
-
 
 
 
