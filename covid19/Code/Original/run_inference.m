8c62948b5447b770099bc3ff1d9d221aeb3a9e80
Kevin Murphy <murphyk@gmail.com>
2020-04-09 17:01:03 -0700

    adding checkpoint code

--- covid19/Code/Original/run_inference.m
+++ covid19/Code/Original/run_inference.m
@@ -1,3 +1,5 @@
+function run_inference()
+
 load('../../Data/Mobility.mat')
 load('../../Data/pop.mat')
 load('../../Data/incidence.mat')
@@ -6,21 +8,25 @@ num_ens = 10;
 seeds = 1:2;
 nseeds = length(seeds);
 param_dist = zeros(6, nseeds);
-tic
 for seedi = 1:nseeds
     seed = seeds(seedi);
+
     fprintf('\n\nrunning seed %d\n', seed)
     % para_post: (num_para,num_ens,num_times,Iter);
     % theta: (num_para,Iter+1)
     % params are stored in this order: beta, mu, theta, Z, alpha, D
-    [para_post,theta] = inference2(M, pop, incidence, num_iter, num_ens);
-    disp(theta) % final iteration of method
+    [~,theta] = inference2(M, pop, incidence, num_iter, num_ens);
+    %disp(theta) % final iteration of method
     param_dist(:,seedi) = theta(:,end);
-end
-toc  % 10 minutes for 5 seeds, 5 iters, 100 samples
-fname = sprintf('param_dist_%dseeds_%dsamples_%diter', nseeds, num_ens, num_iter);
-save(fname, 'param_dist')
+    fname = sprintf('param-dist-seeds-1to%d-samples%d-iter%d', seedi, num_ens, num_iter);
+    save(fname, 'param_dist')
 
+    make_plot(param_dist(:, 1:seedi), fname);
+end
+
+end
+
+function make_plot(param_dist, fname)
 names = {'beta', 'mu', 'theta', 'Z', 'alpha', 'D'};
 figure; 
 for i=1:6
@@ -31,9 +37,10 @@ for i=1:6
     disp(median(dist))
     title(sprintf('%s %5.3f (%4.2f-%4.2f)', names{i}, q(2), q(1), q(3)));
 end
-suptitle(sprintf('Original MLEs over %d seeds, %d samples, %d iter',  nseeds, num_ens, num_iter));
-fname = sprintf('param_boxplot_%dseeds_%dsamples_%diter', nseeds, num_ens, num_iter);
-print(gcf, fname, '-dpng')
+suptitle(sprintf('Sampling distribution for MLEs (original code)\n%s', fname))
+fname = sprintf('%s_boxplot', fname);
+print(gcf, fname, '-dpng');
+end
 
 
 
