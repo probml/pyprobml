5a33a87ae220beb6f2c3c759a1b23fea52f6600d
Kevin Murphy <murphyk@gmail.com>
2020-07-20 21:45:48 -0700

    fixed typpo in sensor_fusion_2d equation h/t JChunX

--- scripts/sensor_fusion_2d.py
+++ scripts/sensor_fusion_2d.py
@@ -2,7 +2,11 @@ import numpy as np
 from scipy.linalg import block_diag
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-from pyprobml_utils import save_fig
+import os
+#from pyprobml_utils import save_fig
+
+figdir = "../figures";
+def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
 
 def gauss_plot2d(mu, sigma, plot_options):
     plt.scatter(mu[0], mu[1],marker="x", c=plot_options['color'])
@@ -23,7 +27,10 @@ def gauss_soft_condition(pmu, py, A, y):
     post['sigma'] = np.linalg.inv(smu_inv + A.T.dot(sy_inv).dot(A))
 
     # reshape is needed to assist in + broadcasting
-    post['mu'] = post['sigma'].dot(A.T.dot(sy_inv).dot(y.reshape(4,1) - py['mu'])) + smu_inv.dot(pmu['mu']).reshape(2,1)
+    ny = py['mu'].shape[0] # 4
+    nm = pmu['mu'].shape[0] # 2 
+    post['mu'] = post['sigma'].dot(A.T.dot(sy_inv).dot(y.reshape(ny,1) - py['mu']) + 
+        smu_inv.dot(pmu['mu']).reshape(nm,1))
     
     # these values are unused
     model = norm(loc=A.dot(pmu['mu']) + py['mu'], scale=py['sigma'] + A.dot(pmu['sigma']).dot(A.T))
@@ -63,7 +70,7 @@ def helper(sigmas):
     py['mu'] = np.zeros((4,1))
     py['sigma'] = block_diag(sigmas[0], sigmas[1])
 
-    #fit model
+
     post, log_evidence = gauss_soft_condition(prior, py, A, y)
     gauss_plot2d(y1, sigmas[0], {"color":"r"})
     gauss_plot2d(y2, sigmas[1], {"color":"g"})
