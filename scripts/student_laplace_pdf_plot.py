75a9fe3ba2a0a73ea82dbac221c7d9a8e3c9273f
Kevin Murphy <murphyk@gmail.com>
2020-10-23 17:56:10 -0700

    plot cauchy

--- scripts/student_laplace_pdf_plot.py
+++ scripts/student_laplace_pdf_plot.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-figdir = os.path.join(os.environ["PYPROBML"], "figures")
+figdir = "../figures"
 def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
 
 from scipy.stats import t, laplace, norm
@@ -11,22 +11,26 @@ from scipy.stats import t, laplace, norm
 x = np.linspace(-4, 4, 100)
 n = norm.pdf(x, loc=0, scale=1)
 l = laplace.pdf(x, loc=0, scale=1 / (2 ** 0.5))
-t = t.pdf(x, df=1, loc=0, scale=1)
+t1 = t.pdf(x, df=1, loc=0, scale=1)
+t2 = t.pdf(x, df=2, loc=0, scale=1)
 
 
 plt.plot(x, n, 'k:',
-        x, t, 'b--',
+        x, t1, 'b--',
+         x, t2, 'g--',
         x, l, 'r-')
-plt.legend(('Gauss', 'Student', 'Laplace'))
+plt.legend(('Gauss', 'Student(dof 1)', 'Student (dof 2)', 'Laplace'))
 plt.ylabel('pdf')
-save_fig('studentLaplacePdf.pdf')
+save_fig('studentLaplacePdf2.pdf')
 plt.show()
 
 plt.figure()
 plt.plot(x, np.log(n), 'k:',
-        x, np.log(t), 'b--',
+        x, np.log(t1), 'b--',
+        x, np.log(t2), 'g--',
         x, np.log(l), 'r-')
 plt.ylabel('log pdf')
-plt.legend(('Gauss', 'Student', 'Laplace'))
-save_fig('studentLaplaceLogpdf.pdf')
+plt.legend(('Gauss', 'Student(dof 1)', 'Student (dof 2)', 'Laplace'))
+#plt.legend(('Gauss', 'Student', 'Laplace'))
+save_fig('studentLaplaceLogpdf2.pdf')
 plt.show()
