46cf8b5e77fffced73462696df008327f0d354a7
Kevin Murphy <murphyk@gmail.com>
2021-01-15 20:37:12 +0000

    add get_machine_info.py to see if GPU is present

---
+++ scripts/get_machine_info.py
@@ -0,0 +1,12 @@
+
+
+import subprocess
+result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE)
+print('GPU info')
+res = result.stdout;
+print(res)
+
+
+from psutil import virtual_memory
+ram_gb = virtual_memory().total / 1e9
+print('!Your runtime has {:.1f} gigabytes of available RAM\n'.format(ram_gb))
