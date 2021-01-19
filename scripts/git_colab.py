fcc1afbf8ad48fe3cd3d43590f9fb0620aa66f40
Kevin Murphy <murphyk@gmail.com>
2021-01-19 13:19:06 -0800

    git_ssh

--- scripts/git_colab.py
+++ scripts/git_colab.py
@@ -7,8 +7,9 @@ import os
 def git_ssh(git_command, email="murphyk@gmail.com", username="probml",
             verbose=False):
     git_command=git_command.replace(r"https://github.com/","git@github.com:")
-    print('executing command:', git_command)
+    print('executing command via ssh:', git_command)
     # copy keys from drive to local .ssh folder
+    print(verbose)
     if verbose:
         print('Copying keys from gdrive to local VM')
     os.system('rm -rf ~/.ssh')
@@ -29,37 +30,3 @@ def git_ssh(git_command, email="murphyk@gmail.com", username="probml",
     os.system('git config --global user.email ""')
     os.system('git config --global user.name ""')
     
-'''   
-# This version uses colab magics.
-def git_colab(
-    git_command, email="murphyk@gmail.com", username="probml", verbose=False):
-  git_command=git_command.replace(r"https://github.com/","git@github.com:")
-  print('executing command:', git_command)
-  # copy keys from drive to local .ssh folder
-  if verbose:
-    print('Copying keys from gdrive to local VM')
-  !rm -rf ~/.ssh/
-  !mkdir ~/.ssh/
-  !cp  -r /content/drive/MyDrive/ssh/* ~/.ssh/
-  if verbose:
-    !ls ~/.ssh/
-  # configure ssh and test it
-  if verbose:
-    print('Setup SSH')
-  !ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-  !ssh -T git@github.com
-  # git commands
-  if verbose:
-    print('Executing git commands')
-  !git config --global user.email $email
-  !git config --global user.name $username
-  !$git_command
-  # cleanup
-  if verbose:
-    print('Cleanup local VM')
-  !rm -r ~/.ssh/
-  !git config --global user.email ""
-  !git config --global user.name ""
-  # check that cleanup worked
-  #!ssh -T git@github.com # should say 'Host key verification failed'
-'''
