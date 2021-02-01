8d64f126542daedfd31b00a7a482293b604df30a
Dustin Smith <Dustin.William.Smith@gmail.com>
2021-02-01 20:46:06 +0700

    moved imports to the top according to PEP8; removed semi-colons; added space around variable names and equals; removed space around method variable name and value; added type hinting to save_fig; change to f string from format

--- scripts/iris_kmeans.py
+++ scripts/iris_kmeans.py
@@ -2,30 +2,31 @@
 # Based on https://github.com/ageron/handson-ml2/blob/master/09_unsupervised_learning.ipynb
 
 
-
-import numpy as np
-import matplotlib.pyplot as plt
 import os
-figdir = "../figures"
-def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
 
+import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-
 from sklearn.datasets import load_iris
-#from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+
+figdir = "../figures"
+
+
+def save_fig(fname: str) -> None: plt.savefig(os.path.join(figdir, fname))
+
 
 iris = load_iris()
 X = iris.data
 
 fig, ax = plt.subplots()
-idx1 = 2; idx2 = 3;
+idx1 = 2
+idx2 = 3
 ax.scatter(X[:, idx1], X[:, idx2], c="k", marker=".")
-ax.set(xlabel = iris.feature_names[idx1])
-ax.set(ylabel = iris.feature_names[idx2])
+ax.set(xlabel=iris.feature_names[idx1])
+ax.set(ylabel=iris.feature_names[idx2])
 save_fig("iris-2d-unlabeled")
 plt.show()
-
 
 K = 3
 y_pred = GaussianMixture(n_components=K, random_state=42).fit(X).predict(X)
@@ -36,11 +37,10 @@ colors = sns.color_palette()[0:K]
 markers = ('s', 'x', 'o', '^', 'v')
 fig, ax = plt.subplots()
 for k in range(0, K):
-  ax.plot(X[y_pred==k, idx1], X[y_pred==k, idx2], color=colors[k], \
-          marker=markers[k], linestyle = 'None', label="Cluster {}".format(k))
-ax.set(xlabel = iris.feature_names[idx1])
-ax.set(ylabel = iris.feature_names[idx2])
+    ax.plot(X[y_pred == k, idx1], X[y_pred == k, idx2], color=colors[k],
+            marker=markers[k], linestyle='None', label=f"Cluster {k}")
+ax.set(xlabel=iris.feature_names[idx1])
+ax.set(ylabel=iris.feature_names[idx2])
 plt.legend(loc="upper left", fontsize=12)
 save_fig("iris-2d-gmm")
 plt.show()
-
