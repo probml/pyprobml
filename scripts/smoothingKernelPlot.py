fcd16a1171ac840ec600f95ef96bb125f91121af
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-13 01:09:17 +0530

    Completed: Pep8 Styling

--- scripts/smoothingKernelPlot.py
+++ scripts/smoothingKernelPlot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plotColors():
 
     colors = ['b', 'r', 'k', 'g', 'c', 'y',
@@ -16,10 +17,15 @@ def plotColors():
 
 
 def box(u): return (1/2)*(abs(u) <= 1)
+
+
 def epa(u): return ((3/4)*(1 - np.power(u, 2))*(abs(u) <= 1))
 
 
-def tri(u): return (70/81) * np.power((np.power(1-abs(u), 3)), 3) * (abs(u) <= 1)
+def tri(u): return (70/81) * \
+    np.power((np.power(1-abs(u), 3)), 3) * (abs(u) <= 1)
+
+
 def gauss(u): return (1/np.sqrt(2*np.pi)) * np.exp(-np.power(u, 2)/2)
 
 
@@ -27,11 +33,11 @@ fns = [box, epa, tri, gauss]
 names = ['Boxcar', 'Epanechnikov', 'Tricube', 'Gaussian']
 
 xs = np.arange(-1.5, 1.501, 0.01)
-[styles, colors, symbols, Str] =  plotColors()
+[styles, colors, symbols, Str] = plotColors()
 
 smoothingKernalPlot = plt.figure()
 
-for i in range (0, len(fns)):
+for i in range(0, len(fns)):
     f = fns[i]
     fx = f(xs)
     b = xs[1]-xs[0]
@@ -39,4 +45,5 @@ for i in range (0, len(fns)):
     smoothingKernalPlot = plt.plot(xs, fx, styles[i]+colors[i])
 
 smoothingKernalPlot = plt.legend(names)
+#plt.savefig('smoothingKernelPlot')
 plt.show()
