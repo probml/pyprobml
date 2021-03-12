eefed2c78c90cc62eefb65f741d8eab2e9c7d7e5
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-12 23:51:56 +0530

    Fixed plotColours()

--- scripts/smoothingKernelPlot.py
+++ scripts/smoothingKernelPlot.py
@@ -1,12 +1,17 @@
 def plotColors():
 
-    colors = ['b' 'r' 'k' 'g' 'c' 'y' 'm', 'r' 'b' 'k' 'g' 'c' 'y' 'm']
-    symbols = ['o' 'x' '*' '>' '<' '^' 'v', '+' 'p' 'h' 's' 'd' 'o' 'x']
-    styles = ['-', ':', '-.', '--', '-', ':', '-.', '--', '-', ':', '-.', '--', '-', ':', '-.', '--']
+    colors = ['b', 'r', 'k', 'g', 'c', 'y',
+              'm', 'r', 'b', 'k', 'g', 'c', 'y', 'm']
+    symbols = ['o', 'x', '*', '>', '<', '^',
+               'v', '+', 'p', 'h', 's', 'd', 'o', 'x']
+    styles = ['-', ':', '-.', '--', '-', ':', '-.',
+              '--', '-', ':', '-.', '--', '-', ':', '-.', '--']
     Str = []
+    print(len(colors))
     for i in range(0, len(colors)):
         Str.append(colors[i] + styles[i])
     return [styles, colors, symbols, Str]
+
 
 [styles, colors, symbols, Str] = plotColors()
 print(Str)
