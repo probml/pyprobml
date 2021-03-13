f13d14365fa54873746d017bcc3d1338595e8003
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-13 17:10:23 +0530

    Fixed: Suggested issues

--- scripts/naiveBayesBowDemo.py
+++ scripts/naiveBayesBowDemo.py
@@ -18,15 +18,7 @@ if True:
     Xtrain = scipy.sparse.csc_matrix.toarray(Xtrain)
     Xtest = data['xtest']
     Xtest = scipy.sparse.csc_matrix.toarray(Xtest)
-
-'''
-else:
-    data = read_mat('data\20news_w100.mat')
-    keys =  list(data.keys())
-    X = random.shuffle(keys)
-    Xtrain = X[1:60, :]; 
-    Xtest = X[61:, :]; 
-'''
+  
 ytrain = data['ytrain']
 ytest = data['ytest']
 model = GaussianNB()
@@ -42,47 +34,6 @@ C = np.unique(data['ytrain']).size
 for i in range(0, C):
     plt.bar(np.arange(0, 600, 1), model.theta_[i, :])
     plt.title('p(xj=1|y='+str(i)+')')
-    # plt.savefig(r'..\figures\naiveBayesBow'+str(i)+'ClassCond')
+    fileName = 'naiveBayesBow'+str(i+1)+'ClassCond'
+    plt.savefig(r'figures/'+fileName)
     plt.show()
-
-N = 5
-Mp = np.zeros((N, C))
-for c in range(0, C):
-    sortedProb = (model.theta_[c, :])
-    sortedProb.sort(axis=0)
-    sortedProb = sortedProb[::-1]
-    print('top '+str(N)+' words for class '+str(c+1))
-    for w in range(0, N):
-        # print(2+'+str(w)+' '+str(sortedProb[w])+' '+ +'\n', w,  sortedProb(w), vocab{ndx(w)})
-        Mp[w, c] = sortedProb[w]
-        # Mw[w,c] = vocab{ndx(w)}
-
-
-mi = mutual_info_classif(Xtrain, ytrain)
-mi.sort(axis=0)
-mi = mi[::-1]
-#sortedMI, ndx = mi
-sortedMI = mi
-print('top '+str(N)+' words sorted by MI')
-Mi = np.zeros((N,))
-for w in (0, N):
-    # fprintf(2,'%2d %6.4f %20s\n', w,  sortedMI(w), vocab{ndx(w)});
-    Mi[w] = sortedMI[w]
-    # Miw{w} = vocab{ndx(w)};
-
-M = np.zeros((N, 6))
-for i in range(0, N):
-    # M[i,1] = Mw[i,1]
-    M[i, 2] = Mp[i, 1]
-    # M{i,3} = Mw[i,2]
-    M[i, 4] = Mp[i, 2]
-    # M[i,5] = Miw[i]
-    M[i, 6] = Mi[i]
-
-'''
-MM = [Mw(:, 1), mat2cellRows(Mp(:, 1)), Mw(:,2),  mat2cellRows(Mp(:, 2)), Miw(:),  mat2cellRows(Mi(:))];
-assert(isequal(M, MM))
-
-colLabels = {'class 1', 'prob', 'class 2', 'prob', 'highest MI', 'MI'};
-latextable(M, 'horiz', colLabels, 'Hline',[1], 'format', '%5.3f');
-'''
