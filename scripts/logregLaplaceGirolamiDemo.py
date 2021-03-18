29e1433a847cec127fc4ced9c67995fbb80cf6d3
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-18 15:26:28 +0530

    Added: logregLaplaceGirolamiPost

--- scripts/logregLaplaceGirolamiDemo.py
+++ scripts/logregLaplaceGirolamiDemo.py
@@ -23,22 +23,18 @@ Step = 0.1
 w1, w2 = np.meshgrid(np.arange(-Range, Range+Step, Step),
                      np.arange(-Range, Step+Range, Step))
 n = w1.shape[0]
-# W=[reshape(w1,n*n,1) reshape(w2,n*n,1)];
 W = np.concatenate((w1.reshape((n*n, 1)), w2.reshape((n*n, 1))), axis=1)
 plt.plot(X[(np.argwhere(t == 1)[:, 0]), 0],
          X[(np.argwhere(t == 1)[:, 0]), 1], 'r.')
 plt.plot(X[(np.argwhere(t == 0)[:, 0]), 0],
          X[(np.argwhere(t == 0)[:, 0]), 1], 'bo')
-# plt.show()
 
 Xgrid = W
 ws = np.array([[3, 1], [4, 2], [5, 3], [7, 3]])
-# print(W.shape)
 
 for ii in range(0, ws.shape[0]):
     w = np.transpose(ws[ii, :])
     pred = 1/(1+np.exp(-np.dot(Xgrid, w)))
-    # print(pred.reshape((n, n)).shape)
     cc = plt.contour(w1, w2, pred.reshape((n, n)), 1)
 
 plt.savefig('../figures/logregLaplaceGirolamiData.png')
@@ -49,13 +45,41 @@ for ii in range(0, ws.shape[0]):
     print(np.linalg.norm(w))
 
 f = np.dot(W, np.transpose(X))
-#print(np.zeros((1, D)).shape)
 Log_Prior = np.log(multivariate_normal.pdf(W, cov=np.eye(D)*alpha))
-#print(np.dot(np.dot(W, np.transpose(X)), t).shape)
-Log_Like = np.dot(np.dot(W, np.transpose(X)), t) - np.sum(np.log(1+np.exp(f)), 1).reshape(((25921, 1)))
-#Log_Joint = Log_Like + Log_Prior
+Log_Like = np.dot(np.dot(W, np.transpose(X)), t) - \
+    np.sum(np.log(1+np.exp(f)), 1).reshape(((25921, 1)))
 
-plt.contour(w1,w2, (-Log_Prior).reshape((n,n)) ,30)
+
+plt.contour(w1, w2, (-Log_Prior).reshape((n, n)), 30)
 plt.title('Log-Prior', fontdict={'size': fs})
 plt.savefig('../figures/logregLaplaceGirolamiPrior.png')
+plt.show()
+
+
+plt.contour(w1, w2, (-Log_Like).reshape((n, n)), 30)
+plt.title('Log-Likelihood', fontdict={'size': fs})
+
+for ii in range(0, ws.shape[0]):
+    w = np.transpose(ws[ii, :])
+    plt.annotate(str(ii+1), xy=(w[0], w[1]))
+
+j = np.argmax(Log_Like)
+i = Log_Like[j][0]
+wmle = W[j, :]
+x2 = 8
+y2 = 3.3
+plt.plot([0,  x2], [0, y2])
+plt.grid()
+plt.savefig('../figures/logregLaplaceGirolamiNLL.png')
+plt.show()
+
+Log_Prior = Log_Prior.reshape((25921, 1))
+Log_Joint = np.add(Log_Like, Log_Prior)
+plt.contour(w1, w2, (-Log_Joint).reshape((n, n)), 30)
+plt.title('Log-Unnormalised Posterior', fontdict={'size': fs})
+j = np.argmax(Log_Joint)
+i = Log_Joint[j][0]
+plt.plot(W[j, 0], W[j, 1], '.', markersize=40)
+plt.grid()
+plt.savefig('../figures/logregLaplaceGirolamiPost.png')
 plt.show()
