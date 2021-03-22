1116be0510b1180c4235a2555fd3e7cb8ed89213
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-03-22 08:18:27 +0530

    Update twoPowerCurves.py

--- scripts/twoPowerCurves.py
+++ scripts/twoPowerCurves.py
@@ -1,17 +1,17 @@
 # Convert twoPowerCurves(Fig 5.15 (b)) to python/JAX
 # Author: Garvit9000c
 
-import jax.numpy as jnp
+import numpy as np
 import matplotlib.pyplot as plt
 
-pi=jnp.pi
+pi=np.pi
 
 #Domain
-x=jnp.arange(-1*pi,pi,0.01)
+x=np.arange(-1*pi,pi,0.01)
 
 #functions
-y1=-1*jnp.cos(x)+0.03
-y2=-1*jnp.cos(2*x/3)
+y1=-1*np.cos(x)+0.03
+y2=-1*np.cos(2*x/3)
 
 plt.ylim(-2,2.5)
 
@@ -27,9 +27,9 @@ plt.arrow(0,-2.5,0,4.5,color='black',width = 0.03)       #y-axis
 plt.arrow(-1*pi,-1.5,2*pi,0,color='black',width = 0.03)  #x-axis
 
 #labels
-plt.text(-0.3, 2.2, '1 - β', fontsize=15) # 1 - β
+plt.text(-0.3, 2.2, r'$1 - \beta$', fontsize=15) # 1 - β
 plt.text(-0.3, 0.5, '1', fontsize=15)     # 1
-plt.text(-0.3, -1.3, 'α', fontsize=15)    # α
+plt.text(-0.3, -1.3, r'$\alpha$', fontsize=15)    # α
 plt.text(-0.34, -1.8, '$θ_0$', fontsize=12)  # θo
 plt.text(3.1, -1.8, 'θ', fontsize=15)     # θ
 plt.text(2.5, -0.2, 'A', fontsize=15)     # A
