4c5009d6f7d6d9a66a933931bc20bab5bf714e30
Ashish Papanai <ashishpapanai00@gmail.com>
2021-03-26 03:25:29 +0530

    Added: Plots

--- scripts/gprDemoArd.py
+++ scripts/gprDemoArd.py
@@ -1,65 +1,113 @@
-import numpy as np
+# Generates Figure 17.2 
+# Author: Ashish Papanai (@ashishpapanai)
+
+import os
+
 import matplotlib.pyplot as plt
-import scipy.linalg
+import numpy as np
+from matplotlib import cm
 
-n = 61**2
-D = 2
-
-r = np.arange(-3, 3.1, 0.1)
-x1, x2 = np.meshgrid(r, r)
-xx1 = x1.reshape((-1,1), order='F')
-xx2 = x2.reshape((-1,1), order='F')
-x = np.hstack((xx1,xx2))
-Q = np.zeros((n, n))
-
-for d in range(D):
-    Q = Q + (np.tile(x[:, d].reshape((n, 1)), (1, n)) - np.tile(x[:, d].T, (n, 1)))**2
-    
-Q = np.exp(-0.5*Q)
+if os.path.isdir('scripts'):
+    os.chdir('scripts')
 
 np.random.seed(37)
+a = 61
+n = 61 ** 2
+D = 2
+x1, x2 = np.meshgrid(np.arange(-3, 3.1, 0.1), np.arange(-3, 3.1, 0.1))
+x = (np.concatenate((x1.reshape(
+    x1.shape[0]*x1.shape[1], 1), x2.reshape(x2.shape[0]*x2.shape[1], 1)), axis=1))
 
-y = scipy.linalg.cholesky(Q+1e-9*np.identity(n)).T.dot(np.random.randn(n,1))
-b = y.reshape((61, 61))
-fig = plt.figure(figsize=(12, 7))
-ax = fig.gca(projection='3d')
-ax.plot_surface(x1, x2, b, lw=0.5, cmap='viridis')
-ax.set_xlabel('input x1')
-ax.set_ylabel('input x2')
-ax.set_zlabel('output y')
-
-ax.view_init(None, 180+50)
+x[:, [0, 1]] = x[:, [1, 0]]
 
 
-np.random.seed(0)
-y = scipy.linalg.cholesky(Q+1e-9*np.identity(n)).T.dot(np.random.randn(n,1))
-#%matplotlib qt
-b = y.reshape((61, 61), order='F')
-fig = plt.figure(figsize=(12, 7))
-ax = fig.gca(projection='3d')
-ax.plot_surface(x1, x2, b, lw=0.5, cmap='viridis')
-ax.set_xlabel('input x1')
-ax.set_ylabel('input x2')
-ax.set_zlabel('output y')
-
-ax.view_init(None, 180+50)
-
+# Plot #1
 Q = np.zeros((n, n))
-L = [1, 5]
-for d in range(D):
-    Q = Q + (np.tile(x[:, d].reshape((n, 1)), (1, n)) - np.tile(x[:, d].T, (n, 1)))**2/L[d]**2
-    
-Q = np.exp(-0.5*Q)
+
+for d in range(0, D):
+    Q = Q + np.power(np.tile((x[:, d]).reshape(n, 1), (1, n)) -
+                     (np.tile((x[:, d]).reshape(1, n), (n, 1))), 2)
 
 
-np.random.seed(0)
-y = scipy.linalg.cholesky(Q+1e-9*np.identity(n)).T.dot(np.random.randn(n,1))
-b = y.reshape((61, 61), order='F')
-fig = plt.figure(figsize=(12, 7))
-ax = fig.gca(projection='3d')
-ax.plot_surface(x1, x2, b, lw=0.5, cmap='viridis')
+Q = np.exp(np.multiply(-0.5, Q))
+
+arr = np.transpose(Q + 1e-9 * np.eye(n))
+y = np.dot(np.linalg.cholesky(arr), np.random.randn(n, 1))
+
+fig1 = plt.figure()
+ax = fig1.gca(projection='3d')
+surf = ax.plot_surface(x1, x2, y.reshape((a, a)), cmap=cm.viridis)
 ax.set_xlabel('input x1')
 ax.set_ylabel('input x2')
 ax.set_zlabel('output y')
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
+ax.set_zlim(-2, 2)
+ax.grid(False)
+plt.savefig('../figures/GPARD.png')
+plt.show()
 
-ax.view_init(None, 180+50)
+
+# Plot #2
+
+np.random.seed(34)
+Q = np.zeros((n, n))
+L1 = 1
+L2 = 5
+
+Q = Q + (np.power(np.tile((x[:, 0]).reshape(n, 1), (1, n)) -
+                  (np.tile((x[:, 0]).reshape(1, n), (n, 1))), 2)) / L1**2
+Q = Q + (np.power(np.tile((x[:, 1]).reshape(n, 1), (1, n)) -
+                  (np.tile((x[:, 1]).reshape(1, n), (n, 1))), 2)) / L2**2
+Q = np.exp(np.multiply(-0.5, Q))
+
+arr = np.transpose(Q + 1e-9 * np.eye(n))
+y = np.dot(np.linalg.cholesky(arr), np.random.randn(n, 1))
+
+fig2 = plt.figure()
+ax = fig2.gca(projection='3d')
+surf = ax.plot_surface(x1, x2, y.reshape((a, a)), cmap=cm.viridis)
+ax.set_xlabel('input x1')
+ax.set_ylabel('input x2')
+ax.set_zlabel('output y')
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
+ax.set_zlim(-2, 2)
+ax.grid(False)
+plt.savefig('../figures/GPARD2.png')
+plt.show()
+
+
+# Plot #3
+
+np.random.seed(34)
+Q = np.zeros((n, n))
+L1 = 1
+L2 = 5
+
+Q = Q + (np.power(np.tile((x[:, 0]).reshape(n, 1), (1, n)) -
+                  (np.tile((x[:, 0]).reshape(1, n), (n, 1))), 2))
+
+Q = Q + (np.power(np.tile((x[:, 0]).reshape(n, 1), (1, n)) -
+                  (np.tile((x[:, 0]).reshape(1, n), (n, 1))), 2))/36
+
+Q = Q + (np.power(np.tile((x[:, 1]).reshape(n, 1), (1, n)) -
+                  (np.tile((x[:, 1]).reshape(1, n), (n, 1))), 2)) / 36
+
+Q = np.exp(np.multiply(-0.5, Q))
+
+arr = np.transpose(Q + 1e-9 * np.eye(n))
+y = np.dot(np.linalg.cholesky(arr), np.random.randn(n, 1))
+
+fig2 = plt.figure()
+ax = fig2.gca(projection='3d')
+surf = ax.plot_surface(x1, x2, y.reshape((a, a)), cmap=cm.viridis)
+ax.set_xlabel('input x1')
+ax.set_ylabel('input x2')
+ax.set_zlabel('output y')
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
+ax.set_zlim(-2, 2)
+ax.grid(False)
+plt.savefig('../figures/GPARDFA.png')
+plt.show()
