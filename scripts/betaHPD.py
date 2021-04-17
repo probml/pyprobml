f4f7ecc3fecbaf8e65962812d91c21954e6b35cb
Ang Ming Liang <angmingliang4017ic@gmail.com>
2021-04-17 15:47:43 +0800

    Converting betaHPD to python

---
+++ scripts/betaHPD.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""BetaHDR.ipynb
+
+Original file is located at
+    https://colab.research.google.com/drive/1hUa4YVvGeebUKZj_n63kAPB8IskaqX_e
+
+"""
+
+import seaborn as sns
+import numpy as np
+from scipy.stats import beta
+from scipy.optimize import fmin
+import matplotlib.pyplot as plt
+sns.set_style("ticks")
+
+
+def HDIofICDF(dist_name, credMass=0.95, **args):
+    '''
+    Warning: This only works for unimodal distributions
+    Source : This was adapted from R to python from John K. Kruschke book, Doing bayesian data analysis,
+    by aloctavodia as part of the answer to the following stackoverflow question[1].
+
+    Reference:
+    1. https://stackoverflow.com/questions/22284502/highest-posterior-density-region-and-central-credible-region
+    '''
+
+    # freeze distribution with given arguments
+    distri = dist_name(**args)
+    # initial guess for HDIlowTailPr
+    incredMass = 1.0 - credMass
+
+    def intervalWidth(lowTailPr):
+        return distri.ppf(credMass + lowTailPr) - distri.ppf(lowTailPr)
+
+    # find lowTailPr that minimizes intervalWidth
+    HDIlowTailPr = fmin(intervalWidth, incredMass, ftol=1e-8, disp=False)[0]
+    # return interval as array([low, high])
+    return list(distri.ppf([HDIlowTailPr, credMass + HDIlowTailPr]))
+
+
+a, b = 3, 9
+alpha = 0.05
+
+l = beta.ppf(alpha / 2, a, b)
+u = beta.ppf(1 - alpha / 2, a, b)
+CI = [l, u]
+
+HPD = HDIofICDF(beta, credMass=0.95, a=a, b=b)
+
+xs = np.linspace(beta.ppf(0.001, a, b),
+                 beta.ppf(0.999, a, b), 40)
+
+names = ['CI', 'HPD']
+linestyles = ['-', '-']
+ints = [CI, HPD]
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
+for i, inter in enumerate(ints):
+
+    # The corresponding pdf for each point of the interval.
+    l, u = inter
+    y1 = beta.pdf(l, a, b)
+    y2 = beta.pdf(u, a, b)
+
+    # The range of the plot
+    plt.xlim(0, 1)
+    plt.ylim(0, 3.5)
+
+    # The title of each plot
+    ax[i].set_title(names[i])
+
+    # The plot of the pdf of the distribution
+    ax[i].plot(xs, beta.pdf(xs, a, b),
+               '-', lw=2, label='beta pdf', color="black")
+
+    # Plotting the intervals
+    ax[i].plot((l, l), (0, y1), color="blue")
+    ax[i].plot((l, u), (y1, y2), color="blue")
+    ax[i].plot((u, u), (y2, 0), color="blue")
