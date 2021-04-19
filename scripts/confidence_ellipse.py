8384d8a823777351277d792cb626db42f0f94418
Kevin Murphy <murphyk@gmail.com>
2021-04-19 10:50:49 -0700

    factor out confidence_ellipse code into separate script

---
+++ scripts/confidence_ellipse.py
@@ -0,0 +1,33 @@
+
+# https://matplotlib.org/devdocs/gallery/statistics/confidence_ellipse.html
+
+
+import numpy as np
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
--- scripts/pcaEmStepByStep.py
+++ scripts/pcaEmStepByStep.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.linalg import svd, eig
 from scipy.linalg import orth
-from matplotlib.patches import Ellipse
-import matplotlib.transforms as transforms
 from matplotlib import pyplot as plt
+
+import pyprobml_utils as pml
+from confidence_ellipse import confidence_ellipse
+
 
 np.warnings.filterwarnings('ignore')
 
@@ -23,31 +25,6 @@ def convergenceTest(fval, previous_fval, threshold=1e-4, warn=False):
     return converged
 
 
-def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
-    if x.size != y.size:
-        raise ValueError("x and y must be the same size")
-
-    cov = np.cov(x, y)
-    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
-
-    ell_radius_x = np.sqrt(1 + pearson)
-    ell_radius_y = np.sqrt(1 - pearson)
-    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
-                      facecolor=facecolor, **kwargs)
-
-    scale_x = np.sqrt(cov[0, 0]) * n_std
-    mean_x = np.mean(x)
-
-    scale_y = np.sqrt(cov[1, 1]) * n_std
-    mean_y = np.mean(y)
-
-    transf = transforms.Affine2D() \
-        .rotate_deg(45) \
-        .scale(scale_x, scale_y) \
-        .translate(mean_x, mean_y)
-
-    ellipse.set_transform(transf + ax.transData)
-    return ax.add_patch(ellipse)
 
 
 n = 25
