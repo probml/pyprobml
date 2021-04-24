1190d1b255d59ddad4b89fee172a61d59637d043
Drishttii <35187749+Drishttii@users.noreply.github.com>
2021-04-24 12:20:11 +0530

    Use jax.numpy consistently (autodiff_demo.py) (#397)
    * use jax.numpy consistently (autodiff_demo.py)
    
    * Use jax.numpy consistently (autodiff_demo.py)
    
    Co-authored-by: Dristi <dristi@Dristis-MacBook-Air.local>

--- scripts/autodiff_demo.py
+++ scripts/autodiff_demo.py
@@ -13,17 +13,16 @@ USE_TF = True
 
 # We make some wrappers around random number generation
 # so it works even if we switch from numpy to JAX
-import numpy as onp # original numpy
+import numpy as np # original numpy
 
 def set_seed(seed):
-    onp.random.seed(seed)
+   jnp.random.seed(seed)
     
 def randn(args):
-    return onp.random.randn(*args)
+    return np.random.randn(*args)
         
 def randperm(args):
-    return onp.random.permutation(args)
-
+    return np.random.permutation(args)
 
 if USE_TORCH:
     import torch
@@ -36,7 +35,7 @@ if USE_TORCH:
         print("Torch cannot find GPU")
     
     def set_seed(seed):
-        onp.random.seed(seed)
+        np.random.seed(seed)
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         
@@ -46,8 +45,8 @@ if USE_TORCH:
    
 if USE_JAX:        
     import jax
-    import jax.numpy as np
-    import numpy as onp
+    import jax.numpy as jnp
+    import numpy as np
     from jax.scipy.special import logsumexp
     from jax import grad, hessian, jacfwd, jacrev, jit, vmap
     from jax.experimental import optimizers
@@ -66,8 +65,7 @@ if USE_TF:
         print(tf.test.gpu_device_name())
     else:
         print("TF cannot find GPU")
-
-
+    tf.compat.v1.enable_eager_execution()
 
 ### Dataset
 import sklearn.datasets
@@ -75,7 +73,7 @@ from sklearn.model_selection import train_test_split
 
 iris = sklearn.datasets.load_iris()
 X = iris["data"]
-y = (iris["target"] == 2).astype(onp.int)  # 1 if Iris-Virginica, else 0'
+y = (iris["target"] == 2).astype(np.int)  # 1 if Iris-Virginica, else 0'
 N, D = X.shape # 150, 4
 
 
@@ -88,14 +86,10 @@ from sklearn.linear_model import LogisticRegression
 # We don't fit the bias term to simplify the comparison below.
 log_reg = LogisticRegression(solver="lbfgs", C=1e5, fit_intercept=False)
 log_reg.fit(X_train, y_train)
-w_mle_sklearn = np.ravel(log_reg.coef_)
+w_mle_sklearn = jnp.ravel(log_reg.coef_)
 
 set_seed(0)
 w = w_mle_sklearn
-
-
-
-
 
 ## Compute gradient of loss "by hand" using numpy
 
@@ -103,20 +97,19 @@ w = w_mle_sklearn
 def BCE_with_logits(logits, targets):
     N = logits.shape[0]
     logits = logits.reshape(N,1)
-    logits_plus = np.hstack([np.zeros((N,1)), logits]) # e^0=1
-    logits_minus = np.hstack([np.zeros((N,1)), -logits])
+    logits_plus = jnp.hstack([np.zeros((N,1)), logits]) # e^0=1
+    logits_minus = jnp.hstack([np.zeros((N,1)), -logits])
     logp1 = -logsumexp(logits_minus, axis=1)
     logp0 = -logsumexp(logits_plus, axis=1)
     logprobs = logp1 * targets + logp0 * (1-targets)
     return -np.sum(logprobs)/N
-
 
 if True:
     # Compute using numpy
     def sigmoid(x): return 0.5 * (np.tanh(x / 2.) + 1)
     
     def predict_logit(weights, inputs):
-        return np.dot(inputs, weights) # Already vectorized
+        return jnp.dot(inputs, weights) # Already vectorized
     
     def predict_prob(weights, inputs):
         return sigmoid(predict_logit(weights, inputs))
@@ -130,7 +123,7 @@ if True:
         X, y = batch
         N = X.shape[0]
         mu = predict_prob(weights, X)
-        g = np.sum(np.dot(np.diag(mu - y), X), axis=0)/N
+        g = jnp.sum(np.dot(np.diag(mu - y), X), axis=0)/N
         return g
     
     y_pred = predict_prob(w, X_test)
@@ -141,12 +134,11 @@ if True:
     print("loss {}".format(loss))
     print("grad {}".format(grad_np))
 
-
 if USE_JAX:
     print("Starting JAX demo")
     grad_jax = grad(NLL)(w, (X_test, y_test))
     print("grad {}".format(grad_jax))
-    assert np.allclose(grad_np, grad_jax)
+    assert jnp.allclose(grad_np, grad_jax)
      
     print("Starting STAX demo")
     # Stax version
@@ -175,14 +167,12 @@ if USE_JAX:
     grad_jax2 = grad(NLL_model)(net_params, net_apply, (X_test, y_test))
     grad_jax3 = grad_jax2[0][0] # layer 0, block 0 (weights not bias)
     grad_jax4 = grad_jax3[:,0] # column vector
-    assert np.allclose(grad_np, grad_jax4)
+    assert jnp.allclose(grad_np, grad_jax4)
     
     print("params {}".format(net_params))
     #print("pred {}".format(y_pred2))
     print("loss {}".format(loss2))
     print("grad {}".format(grad_jax2))
-
-
 
 if USE_TORCH:
     import torch
@@ -197,7 +187,7 @@ if USE_TORCH:
     loss_torch = criterion(y_pred, y_test_tensor)
     loss_torch.backward()
     grad_torch = w_torch.grad[:,0].numpy()
-    assert np.allclose(grad_np, grad_torch)
+    assert jnp.allclose(grad_np, grad_torch)
     
     print("params {}".format(w_torch))
     #print("pred {}".format(y_pred))
@@ -232,7 +222,7 @@ if USE_TORCH:
     loss_torch2.backward()
     params_torch2 = list(model.parameters())
     grad_torch2 = params_torch2[0].grad[0].numpy()
-    assert np.allclose(grad_np, grad_torch2)
+    assert jnp.allclose(grad_np, grad_torch2)
     
     print("params {}".format(w1))
     #print("pred {}".format(y_pred))
@@ -247,18 +237,17 @@ if USE_TF:
     with tf.GradientTape() as tape:
         logits = tf.linalg.matmul(x_test_tf, w_tf)
         y_pred = tf.math.sigmoid(logits)
-        loss_batch = tf.nn.sigmoid_cross_entropy_with_logits(y_test_tf, logits)
+        loss_batch = tf.nn.sigmoid_cross_entropy_with_logits(labels = y_test_tf, logits = logits)
         loss_tf = tf.reduce_mean(loss_batch, axis=0)
     grad_tf = tape.gradient(loss_tf, [w_tf])
     grad_tf = grad_tf[0][:,0].numpy()
-    assert np.allclose(grad_np, grad_tf)
+    assert jnp.allclose(grad_np, grad_tf)
     
     print("params {}".format(w_tf))
     #print("pred {}".format(y_pred))
     print("loss {}".format(loss_tf))
     print("grad {}".format(grad_tf))
     
-
 if False:
     # This no longer runs
     print("Starting TF demo: keras version")
@@ -277,7 +266,7 @@ if False:
         loss_tf2 = tf.reduce_mean(loss_batch2, axis=0)
     grad_tf2 = tape.gradient(loss_tf2, model.trainable_variables)
     grad_tf2 = grad_tf2[0][:,0].numpy()
-    assert np.allclose(grad_np, grad_tf2)
+    assert jnp.allclose(grad_np, grad_tf2)
     
     print("params {}".format(w_tf2))
     print("loss {}".format(loss_tf2))
