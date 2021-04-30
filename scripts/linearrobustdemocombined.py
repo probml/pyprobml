45b67c41ef460ae409065d3545ba4c7e539863e5
Dristi <dristi@Dristis-MacBook-Air.local>
2021-04-30 13:57:26 +0530

    Convert linregRobustDemoCombined to python

--- scripts/linearrobustdemocombined.py
+++ scripts/linearrobustdemocombined.py
@@ -1,18 +1,19 @@
 import pyprobml_utils as pml
-
 import matplotlib.pyplot as plt 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import HuberRegressor
 import numpy as np
 import pandas as pd
 from statsmodels.miscmodels.tmodel import TLinearModel
+from statsmodels.regression.quantile_regression import QuantReg
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 #!pip install statsmodels
 
 """Dataset according to the matlab code and book"""
 
-np.random.seed(1) 
+np.random.seed(1) #1
 x = np.random.uniform(low=0.3, high=1.1, size=(10,)) 
 x = x.flatten()
 x = np.sort(x)
@@ -48,52 +49,32 @@ y_pred1 = model1.predict(x_test)
 
 """Huber"""
 
-reg2 = HuberRegressor(epsilon = 1.0)
+reg2 = HuberRegressor(epsilon = 1)
 
 model2 = reg2.fit(x, y)
 y_pred2 = model2.predict(x_test)
 
 """L1"""
 
-def MAE_weights_updation(m, b, X, Y, learning_rate):
-    m_deriv = 0
-    b_deriv = 0
-    N = len(X)
-    for i in range(N):
-        # Calculate partial derivatives
-        m_deriv += - X[i] * (Y[i] - (m*X[i] + b)) / abs(Y[i] - (m*X[i] + b))
-        b_deriv += -(Y[i] - (m*X[i] + b)) / abs(Y[i] - (m*X[i] + b))
-
-    # Updating in direction of steepest ascent
-    m -= (m_deriv / float(N)) * learning_rate
-    b -= (b_deriv / float(N)) * learning_rate
-
-    return m, b
-
-m_new = 1
-b_new = 1
-learning_rate = 0.1
-epochs = 100
-
-for i in range(epochs):
-  m_old, b_old = m_new, b_new 
-  m_new, b_new = MAE_weights_updation(m_old, b_old, x, y, learning_rate)
-
-y_pred4 = m_new*x_test + b_new
-
-"""Student-t"""
-
 dfx = pd.DataFrame(x, columns = ['x'])
 dfy = pd.DataFrame(y, columns = ['y'])
 exog = sm.add_constant(dfx['x'])
 endog = dfy['y']
+dft = pd.DataFrame(x_test, columns = ['test'])
+
+qrmodel = QuantReg(endog, exog)
+result = qrmodel.fit(q=0.5)
+
+ypred_qr = np.dot(dft, result.params[1]) + result.params[0] #results.predict(dft)
+
+"""Student-t"""
 
 tmodel = TLinearModel(endog, exog)
 results = tmodel.fit(df=0.6)
 
-dft = pd.DataFrame(x_test, columns = ['test'])
-
 ypred_t = np.dot(dft, results.params[1]) + results.params[0] #results.predict(dft)
+
+"""Plot"""
 
 plt.xlim(xmin, xmax)
 plt.ylim(ymin, ymax)
@@ -102,7 +83,7 @@ plt.scatter(x, y, color="none", edgecolor="black")
 plt.plot(x_test, y_pred1, '-.', color='black') #Least squares
 plt.plot(x_test, y_pred2, '--', color='green') #Huber
 plt.plot(x_test, ypred_t, color='red')         #student
-plt.plot(x_test, y_pred4, '--', color='blue')  #Laplace
+plt.plot(x_test, ypred_qr, '--', color='blue') 
 plt.legend(["Least squares", "Huber, \u0394 =1", "Student-t, \u03BD =0.6", "Laplace"])
 pml.save_fig('Robust.png')
 plt.savefig('Robust.png')
