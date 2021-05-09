8d07251c7fd5352229bb1c6b96f3942d2782e613
always-newbie161 <66471669+always-newbie161@users.noreply.github.com>
2021-05-10 01:02:05 +0530

    replaced MCMC(pymc3) with analytic solution

--- scripts/prior_post_pred_binom_pymc3.py
+++ scripts/prior_post_pred_binom_pymc3.py
@@ -1,54 +1,49 @@
-# prior and posterior predctiive for beta binomial
-# fig 1.6 of 'Bayeysian Modeling and Computation'
-
 import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import pymc3 as pm
+from scipy.special import comb, beta
+from scipy.stats import binom
 from scipy import stats
-from scipy.stats import entropy
-from scipy.optimize import minimize
-
-import pyprobml_utils as pml
-
 
 np.random.seed(0)
+
+a_prior, b_prior = 1,1
+
 Y = stats.bernoulli(0.7).rvs(20)
 
+N1,N0 = Y.sum(),len(Y)-Y.sum()
 
-with pm.Model() as model:
-    θ = pm.Beta("θ", 1, 1)
-    y_obs = pm.Binomial("y_obs",n=1, p=θ, observed=Y)
-    trace = pm.sample(1000, cores=1, return_inferencedata=False)
-
-idata = az.from_pymc3(trace)
-
-pred_dists = (pm.sample_prior_predictive(1000, model)["y_obs"],
-              pm.sample_posterior_predictive(idata, 1000, model)["y_obs"])
-
-dist=pred_dists[0]
-print(dist.shape)
-num_success = dist.sum(1)
-print(num_success.shape)
+a_post = a_prior + N1
+b_post = b_prior + N0
 
 
+prior_pred_dist, post_pred_dist = [],[]
+N = 20
+
+for k in range(N+1):
+  post_pred_dist.append(comb(N,k) * beta(k+a_post, N-k+b_post) / beta(a_post, b_post))
+  prior_pred_dist.append(comb(N,k) * beta(k+a_prior, N-k+b_prior) / beta(a_prior, b_prior))
+  
 fig, ax = plt.subplots()
-az.plot_dist(pred_dists[0].sum(1), hist_kwargs={"color":"0.5", "bins":range(0, 22)})
+ax.bar(np.arange(N+1), prior_pred_dist, align='center',color='grey')
 ax.set_title(f"Prior predictive distribution",fontweight='bold')
 ax.set_xlim(-1, 21)
+ax.set_xticks(list(range(N+1)))
+ax.set_xticklabels(list(range(N+1)))
 ax.set_ylim(0, 0.15) 
 ax.set_xlabel("number of success")
 
 fig, ax = plt.subplots()
-az.plot_dist(pred_dists[1].sum(1), hist_kwargs={"color":"0.5", "bins":range(0, 22)})
+ax.bar(np.arange(N+1), post_pred_dist, align='center',color='grey')
 ax.set_title(f"Posterior predictive distribution",fontweight='bold')
 ax.set_xlim(-1, 21)
-ax.set_ylim(0, 0.15) 
+ax.set_xticks(list(range(N+1)))
+ax.set_xticklabels(list(range(N+1)))
+ax.set_ylim(0, 0.15)
 ax.set_xlabel("number of success")
 
 fig, ax = plt.subplots()
-az.plot_dist(θ.distribution.random(size=1000), plot_kwargs={"color":"0.5"},
+az.plot_dist(np.random.beta(a_prior,b_prior,1000), plot_kwargs={"color":"0.5"},
              fill_kwargs={'alpha':1})
 ax.set_title("Prior distribution", fontweight='bold')
 ax.set_xlim(0, 1)
@@ -57,10 +52,12 @@ ax.tick_params(axis='both', pad=7)
 ax.set_xlabel("θ") 
 
 fig, ax = plt.subplots()
-az.plot_dist(idata.posterior["θ"], plot_kwargs={"color":"0.5"},
+az.plot_dist(np.random.beta(a_post,b_post,1000), plot_kwargs={"color":"0.5"},
              fill_kwargs={'alpha':1})
 ax.set_title("Posterior distribution", fontweight='bold')
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 4)
 ax.tick_params(axis='both', pad=7)
 ax.set_xlabel("θ")
+
+plt.show()
