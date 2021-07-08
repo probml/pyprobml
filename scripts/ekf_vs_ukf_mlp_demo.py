90347ca0b9a1b1ed68123dc619ff5b7e66659be3
Gerardo Durán Martín <g.duran@me.com>
2021-07-08 06:09:10 +0100

    refactor: ekf_vs_ukf_mlp_demo
    * Key generation now comes from split
    * Number of hidden units in MLP is now a parameter

--- scripts/ekf_vs_ukf_mlp_demo.py
+++ scripts/ekf_vs_ukf_mlp_demo.py
@@ -9,6 +9,7 @@ import nlds_lib as ds
 import flax.linen as nn
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from functools import partial
 from jax.random import PRNGKey, split, normal, multivariate_normal
 
 plt.rcParams["axes.spines.right"] = False
@@ -26,8 +27,8 @@ def sample_observations(key, f, n_obs, xmin, xmax, x_noise=0.1, y_noise=3.0):
     x, y = jnp.array(X.T)
     return x, y
 
-n_hidden = 6
-def fwd_mlp(W, x):
+
+def mlp(W, x, n_hidden):
     W1 = W[:n_hidden].reshape(n_hidden, 1)
     W2 = W[n_hidden: 2 * n_hidden].reshape(1, n_hidden)
     b1 = W[2 * n_hidden: 2 * n_hidden + n_hidden]
@@ -35,6 +36,8 @@ def fwd_mlp(W, x):
     
     return W2 @ nn.tanh(W1 @ x + b1) + b2
 
+n_hidden = 6
+fwd_mlp = partial(mlp, n_hidden=n_hidden)
 def f(x): return x -10 * jnp.cos(x) * jnp.sin(x) + x ** 3
 def fz(W): return W
 
@@ -46,23 +49,21 @@ fwd_mlp_weights = jax.vmap(fwd_mlp, in_axes=[1, None])
 fwd_mlp_obs_weights = jax.vmap(fwd_mlp_obs, in_axes=[0, None])
 
 
+n_obs = 200
+key = PRNGKey(31415)
+key_sample_obs, key_weights = split(key, 2)
+xmin, xmax = -3, 3
+x, y = sample_observations(key_sample_obs, f, n_obs, xmin, xmax)
+
 sigma = 0.1
+n_hidden = 6
 n_in, n_out = 1, 1
 n_params = n_in * n_hidden + n_hidden * n_out  + n_hidden + n_out
 
-n_params = 1 * n_hidden + n_hidden * 1 + n_hidden + 1
-W0 = jnp.zeros((n_params,))
-
-key = PRNGKey(31415)
-W0 = normal(key, (n_params,)) * sigma
+W0 = normal(key_weights, (n_params,)) * sigma
 Q = jnp.eye(n_params) * sigma ** 2
 R = jnp.eye(1) * 0.9
 
-
-n_obs = 200
-key = PRNGKey(314)
-xmin, xmax = -3, 3
-x, y = sample_observations(key, f, n_obs, xmin, xmax)
 
 ekf = ds.ExtendedKalmanFilter(fz, fwd_mlp, Q, R)
 ekf_mu_hist, ekf_Sigma_hist = ekf.filter(W0, y[:, None], x[:, None])
