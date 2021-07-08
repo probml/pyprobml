77e84edb11d78aba91f853b523f9eb9740419d54
Gerardo Durán Martín <g.duran@me.com>
2021-07-08 06:44:40 +0100

    feat: ekf_vs_ukf_mlp_demo
    * Add __main__ entrypoint
    * Save figures

--- scripts/ekf_vs_ukf_mlp_demo.py
+++ scripts/ekf_vs_ukf_mlp_demo.py
@@ -9,12 +9,10 @@ import nlds_lib as ds
 import flax.linen as nn
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import pyprobml_utils as pml
 from functools import partial
 from numpy.random import shuffle, seed
 from jax.random import PRNGKey, split, normal, multivariate_normal
-
-plt.rcParams["axes.spines.right"] = False
-plt.rcParams["axes.spines.top"] = False
 
 
 def sample_observations(key, f, n_obs, xmin, xmax, x_noise=0.1, y_noise=3.0, shuffle_seed=None):
@@ -41,7 +39,6 @@ def plot_mlp_prediction(key, xobs, yobs, xtest, fw, w, Sw, ax, n_samples=100):
     ax.set_xlim(xobs.min(), xobs.max())
 
 
-
 def mlp(W, x, n_hidden):
     W1 = W[:n_hidden].reshape(n_hidden, 1)
     W2 = W[n_hidden: 2 * n_hidden].reshape(1, n_hidden)
@@ -50,53 +47,59 @@ def mlp(W, x, n_hidden):
     
     return W2 @ nn.tanh(W1 @ x + b1) + b2
 
-def f(x): return x -10 * jnp.cos(x) * jnp.sin(x) + x ** 3
-def fz(W): return W
 
-# *** MLP configuration ***
-n_hidden = 6
-n_in, n_out = 1, 1
-n_params = n_in * n_hidden + n_hidden * n_out  + n_hidden + n_out
-fwd_mlp = partial(mlp, n_hidden=n_hidden)
-# vectorised for multiple observations
-fwd_mlp_obs = jax.vmap(fwd_mlp, in_axes=[None, 0])
-# vectorised for multiple weights
-fwd_mlp_weights = jax.vmap(fwd_mlp, in_axes=[1, None])
-# vectorised for multiple observations and weights
-fwd_mlp_obs_weights = jax.vmap(fwd_mlp_obs, in_axes=[0, None])
+if __name__ == "__main__":
+    plt.rcParams["axes.spines.right"] = False
+    plt.rcParams["axes.spines.top"] = False
+    def f(x): return x -10 * jnp.cos(x) * jnp.sin(x) + x ** 3
+    def fz(W): return W
 
-# *** Generating training and test data ***
-n_obs = 200
-shuffle_seed = 271
-key = PRNGKey(314)
-key_sample_obs, key_weights = split(key, 2)
-xmin, xmax = -3, 3
-x, y = sample_observations(key_sample_obs, f, n_obs, xmin, xmax, shuffle_seed=shuffle_seed)
-xtest = jnp.linspace(x.min(), x.max(), n_obs)
+    # *** MLP configuration ***
+    n_hidden = 6
+    n_in, n_out = 1, 1
+    n_params = n_in * n_hidden + n_hidden * n_out  + n_hidden + n_out
+    fwd_mlp = partial(mlp, n_hidden=n_hidden)
+    # vectorised for multiple observations
+    fwd_mlp_obs = jax.vmap(fwd_mlp, in_axes=[None, 0])
+    # vectorised for multiple weights
+    fwd_mlp_weights = jax.vmap(fwd_mlp, in_axes=[1, None])
+    # vectorised for multiple observations and weights
+    fwd_mlp_obs_weights = jax.vmap(fwd_mlp_obs, in_axes=[0, None])
 
-# *** MLP Training with xKF ***
-sigma = 0.1
-W0 = normal(key_weights, (n_params,)) * sigma
-Q = jnp.eye(n_params) * sigma ** 2
-R = jnp.eye(1) * 0.9
-alpha, beta, kappa = 1.0, 2.0, 3.0 - n_params
-step = -1
+    # *** Generating training and test data ***
+    n_obs = 200
+    shuffle_seed = 271
+    key = PRNGKey(314)
+    key_sample_obs, key_weights = split(key, 2)
+    xmin, xmax = -3, 3
+    x, y = sample_observations(key_sample_obs, f, n_obs, xmin, xmax, shuffle_seed=shuffle_seed)
+    xtest = jnp.linspace(x.min(), x.max(), n_obs)
 
-ekf = ds.ExtendedKalmanFilter(fz, fwd_mlp, Q, R)
-ekf_mu_hist, ekf_Sigma_hist = ekf.filter(W0, y[:, None], x[:, None])
-W_ekf, SW_ekf = ekf_mu_hist[step], ekf_Sigma_hist[step]
+    # *** MLP Training with xKF ***
+    sigma = 0.1
+    W0 = normal(key_weights, (n_params,)) * sigma
+    Q = jnp.eye(n_params) * sigma ** 2
+    R = jnp.eye(1) * 0.9
+    alpha, beta, kappa = 1.0, 2.0, 3.0 - n_params
+    step = -1
 
-ukf = ds.UnscentedKalmanFilter(fz, lambda w, x: fwd_mlp_weights(w, x).T, Q, R, alpha, beta, kappa)
-ukf_mu_hist, ukf_Sigma_hist = ukf.filter(W0, y, x[:, None])
-W_ukf, SW_ukf = ukf_mu_hist[step], ukf_Sigma_hist[step]
+    ekf = ds.ExtendedKalmanFilter(fz, fwd_mlp, Q, R)
+    ekf_mu_hist, ekf_Sigma_hist = ekf.filter(W0, y[:, None], x[:, None])
+    W_ekf, SW_ekf = ekf_mu_hist[step], ekf_Sigma_hist[step]
+
+    ukf = ds.UnscentedKalmanFilter(fz, lambda w, x: fwd_mlp_weights(w, x).T, Q, R, alpha, beta, kappa)
+    ukf_mu_hist, ukf_Sigma_hist = ukf.filter(W0, y, x[:, None])
+    W_ukf, SW_ukf = ukf_mu_hist[step], ukf_Sigma_hist[step]
 
 
-fig, ax = plt.subplots()
-plot_mlp_prediction(key, x, y, xtest, fwd_mlp_obs_weights, W_ekf, SW_ekf, ax)
-ax.set_title("EKF + MLP")
+    fig, ax = plt.subplots()
+    plot_mlp_prediction(key, x, y, xtest, fwd_mlp_obs_weights, W_ekf, SW_ekf, ax)
+    ax.set_title("EKF + MLP")
+    pml.savefig("ekf_mlp.pdf")
 
-fig, ax = plt.subplots()
-plot_mlp_prediction(key, x, y, xtest, fwd_mlp_obs_weights, W_ukf, SW_ukf, ax)
-ax.set_title("UKF + MLP")
+    fig, ax = plt.subplots()
+    plot_mlp_prediction(key, x, y, xtest, fwd_mlp_obs_weights, W_ukf, SW_ukf, ax)
+    ax.set_title("UKF + MLP")
+    pml.savefig("ukf_mlp.pdf")
 
-plt.show()
+    plt.show()
