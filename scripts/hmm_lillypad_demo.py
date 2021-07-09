250e78fa9fe4246285d55f6e7ec997db073a061d
Gerardo Durán Martín <g.duran@me.com>
2021-07-09 07:11:05 +0100

    refactor: hmm_lillypad_demo
    * Define sample_hmm

--- scripts/hmm_lillypad_demo.py
+++ scripts/hmm_lillypad_demo.py
@@ -8,6 +8,22 @@ from scipy.stats import multivariate_normal
 plt.rcParams["axes.spines.right"] = False
 plt.rcParams["axes.spines.top"] = False
 
+def sample_hmm(n_samples, initial_probs, mu_collection, cov_collection):
+    samples_obs = np.zeros((n_samples, 2))
+    samples_state = np.zeros((n_samples,), dtype=np.int)
+    zi = np.random.choice(z, p=initial_probs)
+    N = multivariate_normal(mu_collection[zi], cov_collection[zi])
+    samples_obs[0] = N.rvs()
+    samples_state[0] = zi
+
+    for i in range(1, n_samples):
+        zi = np.random.choice(z, p=A[zi])
+        N = multivariate_normal(mu_collection[zi], cov_collection[zi])
+        samples_obs[i] = N.rvs()
+        samples_state[i] = zi
+    
+    return samples_obs, samples_state
+
 initial_probs = np.array([0.3, 0.2, 0.5])
 
 # transition matrix
@@ -49,32 +65,10 @@ np.random.seed(314)
 n_samples = 50
 z = np.array([0, 1, 2])
 
-# Initial latent variable
-
-def sample_hmm(initial_probs, mu_collection, cov_collection):
-    zi = np.random.choice(z, p=initial_probs)
-    N = multivariate_normal(mu_collection[zi], cov_collection[zi])
-    samples[0] = N.rvs()
-    color = colors[zi]
-    states_sample = [zi]
-    color_sample = [color]
-
-samples = np.zeros((n_samples, 2))
-
-zi = np.random.choice(z, p=initial_probs)
-N = multivariate_normal(mu_collection[zi], cov_collection[zi])
-samples[0] = N.rvs()
-color = colors[zi]
-states_sample = [zi]
-color_sample = [color]
-
-for i in range(1, n_samples):
-    zi = np.random.choice(z, p=A[zi])
-    color = colors[zi]
-    states_sample.append(zi)
-    color_sample.append(color)
-    N = multivariate_normal(mu_collection[zi], cov_collection[zi])
-    samples[i] = N.rvs()
+    
+samples_obs, samples_state = sample_hmm(n_samples, initial_probs, mu_collection, cov_collection)
+print(samples_state)
+color_sample = [colors[i] for i in samples_state]
 
 fig, ax = plt.subplots()
 for k, (mu, S, color) in enumerate(zip(mu_collection, cov_collection, colors)):
@@ -83,13 +77,13 @@ for k, (mu, S, color) in enumerate(zip(mu_collection, cov_collection, colors)):
     ax.contour(*Xgrid, Z, levels=[1], colors=color, linewidths=3)
     ax.text(*(mu + 0.13), f"$k$={k + 1}", fontsize=13, horizontalalignment="right")
     
-ax.plot(*samples.T, c="black", alpha=0.3, zorder=1)
-ax.scatter(*samples.T, c=color_sample, s=30, zorder=2, alpha=0.8)
+ax.plot(*samples_obs.T, c="black", alpha=0.3, zorder=1)
+ax.scatter(*samples_obs.T, c=color_sample, s=30, zorder=2, alpha=0.8)
 ax.set_ylim(0, 1.1)
 
 
 fig, ax = plt.subplots()
-ax.step(range(n_samples), states_sample, where="post", c="black", linewidth=1, alpha=0.6)
-ax.scatter(range(n_samples), states_sample, c=color_sample, zorder=3)
+ax.step(range(n_samples), samples_state, where="post", c="black", linewidth=1, alpha=0.6)
+ax.scatter(range(n_samples), samples_state, c=color_sample, zorder=3)
 
 plt.show()
