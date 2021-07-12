3a2b2ea175ed1365131dbbbe1e9e443474fb88bb
Dristi <dristi@Dristis-MacBook-Air.local>
2021-07-12 09:31:28 +0530

    pml_savefig_batch3

--- scripts/hbayes_binom_covid_uninf_pymc3.py
+++ scripts/hbayes_binom_covid_uninf_pymc3.py
@@ -1,5 +1,3 @@
-
-
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import numpy as np
@@ -11,18 +9,14 @@ import theano.tensor as tt
 
 np.random.seed(123)
 
-
-
 G_samples = np.array([0, 0, 2, 0, 1, 1, 0, 2, 1, 3, 0, 1, 1, 1,
                       54, 0, 0, 1, 3, 0])
 N_samples = np.array([1083, 855, 3461, 657, 1208, 1025, 527,
                       1668, 583, 582, 917, 857,
     680, 917, 53637, 874, 395, 581, 588, 383])
 
-
 G_samples = np.array([1,    0,    3,    0,   1,    5,     11])
 N_samples = np.array([1083, 855, 3461, 657, 1208, 5000, 10000])
-
 
 if False:
     with pm.Model() as model:
@@ -53,21 +47,19 @@ with pm.Model() as model:
     trace = pm.sample(1000)
     
 az.plot_trace(trace)
-plt.savefig('../figures/hbayes_binom_covid_trace.png', dpi=300)
+plt.savefig('hbayes_binom_covid_trace.pdf', dpi=300)
 
 print(az.summary(trace))
 
 axes = az.plot_forest(
     trace, var_names='θ', hdi_prob=0.95, combined=False, colors='cycle')
 y_lims = axes[0].get_ylim()
-plt.savefig('../figures/hbayes_binom_covid_forest.png', dpi=300)
-
+pml.savefig('hbayes_binom_covid_forest.pdf')
 
 J = len(N_samples)
 post_mean = np.zeros(J)
 samples = trace['θ']
 post_mean = np.mean(samples, axis=0)
-
 
 alphas = trace['alpha']
 betas = trace['beta']
@@ -84,9 +76,6 @@ pooled_mle = np.sum(G_samples) / np.sum(N_samples)
 
 print('pooled mle')
 print(pooled_mle)
-
-
-
 
 fig, axs = plt.subplots(4,1, figsize=(8,8))
 axs = np.reshape(axs, 4)
@@ -107,4 +96,4 @@ ax = axs[3]
 ax.bar(xs, post_mean)
 ax.hlines(hyper_mean, 0, J, 'r', lw=3)
 ax.set_title('posterior mean (red line = hparam)')
-plt.savefig('../figures/hbayes_binom_covid_barplot.png', dpi=300)
+pml.savefig('hbayes_binom_covid_barplot.pdf')
