c43ad2f8b5a7529f893c9b326af69914ea51ee57
Dristi <dristi@Dristis-MacBook-Air.local>
2021-07-12 09:28:01 +0530

    pml_savefig_batch2

--- scripts/poisson_dist_plot.py
+++ scripts/poisson_dist_plot.py
@@ -1,12 +1,7 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-figdir = os.path.join(os.environ["PYPROBML"], "figures")
-def save_fig(fname): plt.savefig(os.path.join(figdir, fname))
-
-
+import pyprobml_utils as pml
 from scipy.stats import poisson
 
 for l in [1.0, 10.0]:
@@ -15,5 +10,5 @@ for l in [1.0, 10.0]:
     plt.bar(np.arange(30), probabilities)
     plt.xticks(np.arange(0, 30, 5) + 0.4, np.arange(0, 30, 5))
     plt.title(r'$Poi (\lambda = %.2f)$' % l)
-    save_fig('poissonPlotDemo_%s.png' % l)
+    pml.savefig('poissonPlotDemo_%s.png' % l)
 plt.show()
