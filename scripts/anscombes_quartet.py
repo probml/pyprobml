b334f3d83b18ddbd389f057f3b21be8e93608eb4
Kevin P Murphy <murphyk@gmail.com>
2021-07-14 11:29:05 -0700

    pinwheel

--- scripts/anscombes_quartet.py
+++ scripts/anscombes_quartet.py
@@ -2,7 +2,6 @@
 # Author: Drishtii
 
 import seaborn as sns
-import pandas as pd
 import matplotlib.pyplot as plt
 import pyprobml_utils as pml
 
@@ -18,14 +17,11 @@ for name in names:
     print(name)
     ndx = df['dataset']==name
     df2 = df[ndx]
-    #fig, ax   = plt.subplots()
-    #ax.scatter(df2["x"], df2["y"])
-    #ax.set_title(name)
     lm = sns.lmplot(x="x", y="y", data=df2, ci=None, truncate=False)
     ax = plt.gca()
     ax.set_xlim(0, 20)
     ax.set_ylim(0, 14)
-    mx = df2['x'].to_numpy().mean(); my = df2['y'].to_numpy().mean();
+    mx = df2['x'].to_numpy().mean(); my = df2['y'].to_numpy().mean()
     ax.set_title(f'{name}, mx={mx:0.3f}, my={my:0.3f}', fontsize=12)
     print(df2[['x', 'y']].agg(['count', 'mean', 'var']))
     pml.savefig(f"anscombes_quartet_{name}.pdf")
