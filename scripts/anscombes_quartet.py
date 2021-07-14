9fac1e37711211fd283dc17aec8b2857d90aec6f
Kevin P Murphy <murphyk@gmail.com>
2021-07-13 22:39:21 -0700

    tweak plots

--- scripts/anscombes_quartet.py
+++ scripts/anscombes_quartet.py
@@ -6,13 +6,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pyprobml_utils as pml
 
-# Anscombe's quartet plot
 sns.set_theme(style="ticks")
 df = sns.load_dataset("anscombe")
-g = sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df, col_wrap=2, ci=None, palette="muted", height=4, scatter_kws={"s": 50, "alpha": 1}, legend_out=True, truncate=False)
+g = sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df, col_wrap=4, ci=None, palette="muted",
+               height=4, scatter_kws={"s": 50, "alpha": 1}, legend_out=True, truncate=False)
 g.set(xlim=(2.5, 20.5 ))
 pml.savefig("anscombes_quartet.pdf")
 
-# Stats of all 4 datasets:
-df[['x', 'y']].agg(['count', 'mean', 'var'])
+names = df['dataset'].unique()
+for name in names:
+    print(name)
+    ndx = df['dataset']==name
+    df2 = df[ndx]
+    #fig, ax   = plt.subplots()
+    #ax.scatter(df2["x"], df2["y"])
+    #ax.set_title(name)
+    lm = sns.lmplot(x="x", y="y", data=df2, ci=None, truncate=False)
+    ax = plt.gca()
+    ax.set_xlim(0, 20)
+    ax.set_ylim(0, 14)
+    mx = df2['x'].to_numpy().mean(); my = df2['y'].to_numpy().mean();
+    ax.set_title(f'{name}, mx={mx:0.3f}, my={my:0.3f}', fontsize=12)
+    print(df2[['x', 'y']].agg(['count', 'mean', 'var']))
+    pml.savefig(f"anscombes_quartet_{name}.pdf")
+
+
+
 
--- scripts/datasaurus_dozen.py
+++ scripts/datasaurus_dozen.py
@@ -12,18 +12,30 @@ df = pd.read_csv(url, sep='\t' )
 df1 = df[df['dataset'] == 'dino']
 df2 = df[df['dataset'] != 'dino']
 
-# Plotting separately:
+# Plotting dinasour separately:
 
 sns.lmplot(x="x", y="y", col="dataset", fit_reg=False, hue="dataset", data=df1, col_order = ['dino', ' ', ' ', ' '], col_wrap=4, ci=0, height=4) # 'away', 'h_lines', 'v_lines', 'x_shape', 'star', 'hlines', 'dots', 'circle', 'bullseye', 'slant_up', 'slant_down', 'wide_lines'
 pml.savefig("dino.pdf")
 sns.lmplot(x="x", y="y", col="dataset", fit_reg=False, hue="dataset", data=df2, col_wrap=4, ci=0, height=4)
 pml.savefig("datasaurus.pdf")
 
-# Plotting altogether:
-
-sns.lmplot(x="x", y="y", col="dataset", fit_reg=False, hue="dataset", data=df, col_order = ['dino', 'None ', 'None', 'None', 'away', 'h_lines', 'v_lines', 'x_shape', 'star', 'high_lines', 'dots', 'circle', 'bullseye', 'slant_up', 'slant_down', 'wide_lines'], col_wrap=4, ci=0, height=4) # 
+# Plotting 13 in a 4x4 grid
+sns.lmplot(x="x", y="y", col="dataset", fit_reg=False, hue="dataset", data=df,
+           col_order = ['dino', 'None ', 'None', 'None',
+                        'away', 'h_lines', 'v_lines', 'x_shape',
+                        'star', 'high_lines','dots', 'circle',
+                        'bullseye', 'slant_up', 'slant_down', 'wide_lines'], col_wrap=4, ci=0, height=4) #
 pml.savefig("datasaurus_together.pdf")
+
+# Plot 12 total, leaving out away
+sns.lmplot(x="x", y="y", col="dataset", fit_reg=False, hue="dataset", data=df,
+           col_order = ['dino',  'h_lines', 'v_lines', 'x_shape',
+                        'star', 'high_lines', 'dots', 'circle',
+                        'bullseye', 'slant_up', 'slant_down', 'wide_lines'],
+           col_wrap=4, ci=0, height=4) #
+pml.savefig("datasaurus12.pdf")
+
 
 # Stats of all 12 datasets
 datasets = df.groupby('dataset')
-datasets.agg(['count', 'mean', 'var'])
+print(datasets.agg(['count', 'mean', 'var']))
