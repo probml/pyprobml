cd5055ac6fa32fdf8e7bdf355de6d3f45410a708
Drishttii <35187749+Drishttii@users.noreply.github.com>
2021-07-14 10:23:03 +0530

    anscombes quartet (#572)
    * anscombes quartet
    
    * Update anscombes_quartet.py
    
    Co-authored-by: Dristi <dristi@Dristis-MacBook-Air.local>

---
+++ scripts/anscombes_quartet.py
@@ -0,0 +1,18 @@
+# Anscombe's quartet 
+# Author: Drishtii
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import pyprobml_utils as pml
+
+# Anscombe's quartet plot
+sns.set_theme(style="ticks")
+df = sns.load_dataset("anscombe")
+g = sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df, col_wrap=2, ci=None, palette="muted", height=4, scatter_kws={"s": 50, "alpha": 1}, legend_out=True, truncate=False)
+g.set(xlim=(2.5, 20.5 ))
+pml.savefig("anscombes_quartet.pdf")
+
+# Stats of all 4 datasets:
+df[['x', 'y']].agg(['count', 'mean', 'var'])
+
