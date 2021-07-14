c56c60397578f7df304411ada9678b2d24d8a546
Drishttii <35187749+Drishttii@users.noreply.github.com>
2021-07-14 09:32:22 +0530

    Update anscombes_quartet.py

--- scripts/anscombes_quartet.py
+++ scripts/anscombes_quartet.py
@@ -11,7 +11,7 @@ sns.set_theme(style="ticks")
 df = sns.load_dataset("anscombe")
 g = sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df, col_wrap=2, ci=None, palette="muted", height=4, scatter_kws={"s": 50, "alpha": 1}, legend_out=True, truncate=False)
 g.set(xlim=(2.5, 20.5 ))
-pml.savefig("anscombes_quartet")
+pml.savefig("anscombes_quartet.pdf")
 
 # Stats of all 4 datasets:
 df[['x', 'y']].agg(['count', 'mean', 'var'])
