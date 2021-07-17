30558382d8080b4a014602cd6bd95ee1a7e472be
Kevin P Murphy <murphyk@gmail.com>
2021-07-16 17:22:10 -0700

    clenup scripts

--- scripts/matlab/rng.m
+++
@@ -1,6 +0,0 @@
-% based on tip at http://sachinashanbhag.blogspot.com/2012/09/setting-up-random-number-generator-seed.html
-% forcing old generators
-function rng(x)
-  randn("seed",x)
-  rand("seed",x)
-end
