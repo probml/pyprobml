d61680b7606184a423c75b12ec465b36204b9ab9
Ang Ming Liang <angmingliang4017ic@gmail.com>
2021-08-02 18:18:49 +0800

    Update assembler.py

--- scripts/vae/assembler.py
+++ scripts/vae/assembler.py
@@ -3,12 +3,20 @@ import yaml
 from functools import partial
 from models.base_vae import VAE
 
-def assembler(vae_name, config):
+def assembler(config):
+    # Check config is valid
+    vae_name = config["exp_params"]["model_name"]
+    assert config["encoder_params"]["latent_dim"] == config["decoder_params"]["latent_dim"]
+
     componets = importlib.import_module(f"models.{vae_name}")
     encoder = componets.Encoder(**config["encoder_params"])
     decoder = componets.Decoder(**config["decoder_params"])
     loss = partial(componets.loss, config["loss_params"])
-    vae = VAE(loss, encoder, decoder)
+    
+    # Assemble my model
+    vae = VAE(vae_name, loss, encoder, decoder)
+    vae = VAEModule(vae, config["exp_params"]["LR"], config["encoder_params"]["latent_dim"])
+
     return vae
     
 if __name__ == "__main__":
