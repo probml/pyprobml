b76a271bdf7328732fb7d88d6b396f593ca6270a
Gerardo Durán Martín <g.duran@me.com>
2021-08-05 15:55:30 +0100

    feat: pendulum_1d_demo.py
    * Styling color for outlier data
    * change initial configuration
    * add logs

--- scripts/pendulum_1d_demo.py
+++ scripts/pendulum_1d_demo.py
@@ -16,9 +16,9 @@ from jax.ops import index_update
 plt.rcParams["axes.spines.right"] = False
 plt.rcParams["axes.spines.top"] = False
 
-def plot_filter_true(ax, time, estimate, obs, ground_truth, label):
+def plot_filter_true(ax, time, estimate, obs, ground_truth, label, colors="tab:blue"):
     ax.plot(time, estimate, c="black", label=label)
-    ax.scatter(time, obs, s=10, c="none", edgecolors="tab:blue")
+    ax.scatter(time, obs, s=10, c="none", edgecolors=colors)
     ax.plot(time, ground_truth, c="gray", linewidth=8, alpha=0.5,
             label="true angle")
     ax.legend()
@@ -35,7 +35,7 @@ def fx(x):
 # *** Define initial configuration ***
 g = 10
 dt = 0.015
-qc = 0.5
+qc = 0.06
 Q = jnp.array([
     [qc * dt ** 3 / 3, qc * dt ** 2 / 2],
     [qc * dt ** 2 / 2, qc * dt]
@@ -45,7 +45,7 @@ fx_vmap = jax.vmap(fx)
 fz_vec = jax.vmap(lambda x: fz(x, g=g, dt=dt))
 
 nsteps = 200
-Rt = jnp.eye(1) * 0.05
+Rt = jnp.eye(1) * 0.02
 x0 = jnp.array([1.5, 0.0]).astype(float)
 time = jnp.arange(0, nsteps * dt, dt)
 
@@ -61,6 +61,7 @@ sample_obs_noise = sample_obs.copy()
 samples_map = random.bernoulli(key_noisy, 0.5, (nsteps,)) 
 replacement_values = random.uniform(key_values, (samples_map.sum(),), minval=-2, maxval=2)
 sample_obs_noise = index_update(sample_obs_noise.ravel(), samples_map, replacement_values)
+colors = ["tab:red" if samp else "tab:blue" for samp in samples_map]
 
 
 # *** Perform filtering ****
@@ -70,13 +71,15 @@ ekf = ds.ExtendedKalmanFilter.from_base(model)
 ukf = ds.UnscentedKalmanFilter.from_base(model, alpha, beta, kappa)
 particle_filter = ds.BootstrapFiltering(fz_vec, fx_vmap, Q, Rt)
 
+print("Filtering data...")
 ekf_mean_hist, ekf_Sigma_hist = ekf.filter(x0, sample_obs)
 ukf_mean_hist, ukf_Sigma_hist = ukf.filter(x0, sample_obs)
 pf_mean_hist = particle_filter.filter(key_pf, x0, sample_obs, nsamples=4_000, Vinit=Vinit)
 
+print("Filtering outlier data...")
 ekf_perturbed_mean_hist, ekf_Sigma_hist = ekf.filter(x0, sample_obs_noise)
 ukf_perturbed_mean_hist, ukf_Sigma_hist = ukf.filter(x0, sample_obs_noise)
-pf_perturbed_mean_hist = particle_filter.filter(key_pf, x0, sample_obs_noise, nsamples=5_000)
+pf_perturbed_mean_hist = particle_filter.filter(key_pf, x0, sample_obs_noise, nsamples=2_000)
 
 pf_estimate = fx_vmap(pf_mean_hist)
 pf_perturbed_estimate = fx_vmap(pf_perturbed_mean_hist)
@@ -106,15 +109,18 @@ plot_filter_true(ax, time, pf_estimate, sample_obs, ground_truth, "Bootstrap PF"
 pml.savefig("pendulum_pf_1d_demo.pdf")
 
 fig, ax = plt.subplots()
-plot_filter_true(ax, time, pf_perturbed_estimate, sample_obs_noise, ground_truth, "Bootstrap PF (noisy)")
+plot_filter_true(ax, time, pf_perturbed_estimate, sample_obs_noise,
+                 ground_truth, "Bootstrap PF (noisy)", colors=colors)
 pml.savefig("pendulum_pf_noisy_1d_demo.pdf")
 
 fig, ax = plt.subplots()
-plot_filter_true(ax, time, ekf_perturbed_estimate, sample_obs_noise, ground_truth, "Extended KF (noisy)")
+plot_filter_true(ax, time, ekf_perturbed_estimate, sample_obs_noise,
+                 ground_truth, "Extended KF (noisy)", colors=colors)
 pml.savefig("pendulum_ekf_noisy_1d_demo.pdf")
 
 fig, ax = plt.subplots()
-plot_filter_true(ax, time, ukf_perturbed_estimate, sample_obs_noise, ground_truth, "Unscented KF (noisy)")
+plot_filter_true(ax, time, ukf_perturbed_estimate, sample_obs_noise,
+                 ground_truth, "Unscented KF (noisy)", colors=colors)
 pml.savefig("pendulum_ukf_noisy_1d_demo.pdf")
 
 plt.show()
