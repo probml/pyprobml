b48b982f3a0e9094ecbf9d398695d236e7033e1d
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-08-11 21:55:37 +0300

    Implement mix_gauss_lib and its demos

---
+++ scripts/mix_gauss_em_faithful.py
@@ -0,0 +1,72 @@
+'''
+Visualize fitting a mixture of Gaussians by em algorithm to the old faithful dataset
+reproduce Bishop fig 9.8
+Author: Gerardo Durán-Martín, Aleyna Kara(@karalleyna)
+'''
+import numpy as np
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import pyprobml_utils as pml
+
+from mix_gauss_lib import GMM
+from matplotlib.colors import ListedColormap
+
+# 64-bit precision is needed to attain the same results when scipy.stats.multivariate_normal is used.
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+def create_colormap():
+    # Creates color map
+    N = 256
+    vals = np.ones((N, 4))
+    vals[:, 0] = np.linspace(31 / 256, 214 / 256, N)
+    vals[:, 1] = np.linspace(119 / 256, 39 / 256, N)
+    vals[:, 2] = np.linspace(180 / 256, 40 / 256, N)
+    cmap = ListedColormap(vals)
+    return cmap
+
+def main():
+    cmap = create_colormap()
+    colors = ["tab:red", "tab:blue"]
+
+    observations = np.loadtxt("../data/faithful.txt")
+    # Normalize data
+    observations = (observations - observations.mean(axis=0)) / (observations.std(axis=0))
+    # Initial configuration
+
+    mixing_coeffs = jnp.array([0.5, 0.5])
+
+    means = jnp.vstack([jnp.array([-1.5, 1.5]),
+                       jnp.array([1.5, -1.5])])
+
+    covariances = jnp.array([jnp.eye(2) * 0.1,
+                            jnp.eye(2) * 0.1])
+
+    gmm = GMM(mixing_coeffs, means, covariances)
+    num_of_iters = 50
+    history = gmm.fit_em(observations, num_of_iters=num_of_iters)
+    ll_hist, mix_dist_probs_hist, comp_dist_loc_hist, comp_dist_cov_hist, responsibility_hist = history
+
+    # Create grid-plot
+    hist_index = [0, 10, 25, 30, 35, 40]
+    fig, ax = plt.subplots(2, 3)
+    ax = ax.ravel()
+
+    for idx, axi in zip(hist_index, ax):
+        means = comp_dist_loc_hist[idx]
+        covariances = comp_dist_cov_hist[idx]
+        responsibility = responsibility_hist[idx]
+
+        if idx == 0:
+            responsibility = np.ones_like(responsibility)
+
+        color_map = cmap if idx > 0 else "Dark2"
+        gmm.plot(observations, means, covariances, responsibility, cmap=color_map, colors=colors, ax=axi)
+        axi.set_title(f"Iteration {idx}")
+
+    plt.tight_layout()
+    pml.savefig('../figures/gmm_faithful.pdf')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/mix_gauss_mle_vs_map_jax.py
@@ -0,0 +1,106 @@
+# Demonstrate failure of MLE for GMMs in high-D case, whereas MAP works
+# Based on: https://github.com/probml/pmtk3/blob/master/demos/mixGaussMLvsMAP.m
+# Author: Gerardo Durán-Martín, Aleyna Kara(@karalleyna)
+
+import jax.numpy as jnp
+from jax.random import PRNGKey, split, normal
+from jax import tree_multimap
+
+import matplotlib.pyplot as plt
+from numpy.linalg import cholesky
+from mix_gauss_lib import GMM
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def fill_cov(S, dim):
+    m, m = S.shape
+    S_eye = jnp.identity(dim - m)
+    S_fill = jnp.zeros((m, dim - m))
+    S_fill_left = jnp.vstack((S_fill, S_eye))
+    S_final = jnp.vstack((S, S_fill.T))
+    S_final = jnp.hstack((S_final, S_fill_left))
+    return S_final
+
+def init_sigma(sigma_bases, test_dims):
+  sigmas = []
+  for dim in test_dims:
+    Sigma1_base, Sigma2_base, Sigma3_base = sigma_bases
+    Sigma1 = fill_cov(Sigma1_base, dim)
+    Sigma2 = fill_cov(Sigma2_base, dim)
+    Sigma3 = fill_cov(Sigma3_base, dim)
+
+    Sigma = jnp.stack((Sigma1, Sigma2, Sigma3), axis=0)
+    sigmas.append(Sigma)
+  return sigmas
+
+def init_samples(mu_base, sigmas, test_dims, keys, n_samples):
+  res = []
+  for (Sigma, dim), key in zip(zip(sigmas, test_dims), keys):
+    mu = jnp.hstack([mu_base, jnp.zeros((3, dim - 2))])
+    R = cholesky(Sigma)
+    samples = jnp.ones((n_samples, 1, 1)) * mu[None, ...]
+
+    noise = normal(key, (n_samples, dim))
+    noise = jnp.einsum("kjm,nj->nkm", R, noise)
+
+    samples = samples + noise
+    samples = samples.reshape(-1, dim)
+    res.append(samples)
+  return res
+
+def attempt_em_fit(X, Sigma, pi, dim, k=3, n_attempts=3):
+    N, M = X.shape
+    eta = M + 2
+    n_success_ml = 0
+    n_success_map = 0
+    S = X.std(axis=0)
+    S = jnp.diag(S ** 2) / (k ** (1 / M))
+    for n in range(n_attempts):
+        mu = normal(PRNGKey(n), (k, dim))
+        try:
+            gmm = GMM(pi, mu, Sigma)
+            gmm.fit_em(X, num_of_iters=5)
+            n_success_ml += 1
+        except Exception as E:
+            print(str(E))
+        try:
+            gmm = GMM(pi, mu, Sigma)
+            gmm.fit_em(X, num_of_iters=5, S=S, eta=eta)
+            n_success_map += 1
+        except Exception as E:
+            print(str(E))
+    pct_ml = n_success_ml / n_attempts
+    pct_map = n_success_map / n_attempts
+    return [1-pct_ml, 1-pct_map]
+
+rng_key = PRNGKey(0)
+plt.rcParams["axes.spines.right"] = False
+plt.rcParams["axes.spines.top"] = False
+
+n_comps = 3
+pi = jnp.ones((n_comps, )) / n_comps
+hist_ml, hist_map = [], []
+
+test_dims = jnp.arange(10, 60, 10)
+keys = split(rng_key, 10)
+
+n_samples = 150
+mu_base = jnp.array([[-1, 1], [1, -1], [3, -1]])
+
+Sigma1_base = jnp.array([[1, -0.7], [-0.7, 1]])
+Sigma2_base = jnp.array([[1, 0.7], [0.7, 1]])
+Sigma3_base = jnp.array([[1, 0.9], [0.9, 1]])
+sigmas = init_sigma((Sigma1_base, Sigma2_base, Sigma3_base), test_dims)
+
+samples = init_samples(mu_base, sigmas, test_dims, keys, n_samples)
+hist_ml, hist_map = jnp.array(tree_multimap(lambda X, Sigma, dim: attempt_em_fit(X, Sigma, pi, dim),
+                                            samples, sigmas, test_dims.tolist())).T
+
+fig, ax = plt.subplots()
+ax.plot(test_dims, hist_ml, c="tab:red", marker="o", label="MLE")
+ax.plot(test_dims, hist_map, c="black", marker="o", linestyle="--", label="MAP")
+ax.set_xlabel("dimensionality")
+ax.set_ylabel("fraction of times EM for GMM fails")
+ax.legend()
+plt.show()
