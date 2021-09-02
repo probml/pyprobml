ad831c10e6f40b293aae63cbe05ef2a3705e2d55
Gerardo Durán-Martín <g.duran@me.com>
2021-09-02 03:32:54 +0000

    style: thompson_sampling_bernoulli.py

--- scripts/thompson_sampling_bernoulli.py
+++ scripts/thompson_sampling_bernoulli.py
@@ -11,7 +11,7 @@ from jax.nn import one_hot
 from jax.scipy.stats import beta
 
 def thompson_sampling_step(state, key):
-    alphas, betas, probs = state
+    alphas, betas, reward_per_arm = state
     keys = key
     K = len(alphas)
     
@@ -19,14 +19,14 @@ def thompson_sampling_step(state, key):
     # (Sample from the policy distribution)
     theta_t = random.beta(key, alphas, betas).argmax()
     # Pull the arm and observe reward (either 1 or 0)
-    reward = random.bernoulli(key, probs[theta_t])
+    reward = random.bernoulli(key, reward_per_arm[theta_t])
     
     # Update policy distribution
     ind_vector = one_hot(theta_t, K)
     alphas_posterior = alphas + ind_vector * reward
     betas_posterior = betas + ind_vector * (1 - reward)
     
-    return (alphas_posterior, betas_posterior, probs), (alphas_posterior, betas_posterior)
+    return (alphas_posterior, betas_posterior, reward_per_arm), (alphas_posterior, betas_posterior)
 
 
 p_range = jnp.linspace(0, 1, 100)
@@ -36,14 +36,13 @@ colors = [f"tab:{color}" for color in colors]
 T = 200
 key = random.PRNGKey(314)
 keys = random.split(key, T)
-probs = jnp.array([0.65, 0.4, 0.5, 0.9])
-#probs = jnp.ones(5) * 0.5
-K = len(probs)
+reward_per_arm = jnp.array([0.65, 0.4, 0.5, 0.9])
+K = len(reward_per_arm)
 
 alpha_priors = jnp.ones(K) * 1
 beta_priors = jnp.ones(K) * 1
 
-init_state = (alpha_priors, beta_priors, probs)
+init_state = (alpha_priors, beta_priors, reward_per_arm)
 posteriors, hist = jax.lax.scan(thompson_sampling_step, init_state, keys)
 alpha_posterior, beta_posterior, _ = posteriors
 alpha_hist, beta_hist = hist
@@ -60,7 +59,7 @@ for t in times:
         axi.set_xlim(0, 1)
         n_pos = alpha_hist[t, k].item() - 1
         n_trials = beta_hist[t, k].item() + n_pos - 1
-        axi.set_title(f"t={t+1}\np={probs[k]:0.2f}\n{n_pos:.0f}/{n_trials:.0f}")
+        axi.set_title(f"t={t+1}\np={reward_per_arm[k]:0.2f}\n{n_pos:.0f}/{n_trials:.0f}")
         pml.savefig(f"thompson_sampling_bernoulli_w{k}_t{t}.pdf")
         plt.tight_layout()
 plt.show()
