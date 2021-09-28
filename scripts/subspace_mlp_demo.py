e88769a5b8895cc8a607017a36c983e9d50dbebf
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-09-28 13:39:24 +0300

    Update subspace_mlp_demo.py

--- scripts/subspace_mlp_demo.py
+++ scripts/subspace_mlp_demo.py
@@ -11,7 +11,6 @@
 import superimport
 
 import jax
-import optax
 import jax.numpy as jnp
 import flax.linen as nn
 import numpy as np
@@ -62,7 +61,7 @@ def cross_entropy_loss(params, batch):
 
 
 @jax.jit
-def normal_accuracy(params,batch):
+def normal_accuracy(params, batch):
     logits = MLP().apply({"params": params}, batch["X"])
     logits = jax.nn.log_softmax(logits)
     return jnp.mean(jnp.argmax(logits, -1) == batch["y"])
@@ -72,7 +71,6 @@ def normal_accuracy(params,batch):
 def convert_params_from_subspace_to_full(params_subspace, projection_matrix, params_full_init):
     """
     Project the subspace model weights onto the full model weights
-
     Parameters
     ----------
     params_subspace: jnp.ndarray(d)
@@ -81,7 +79,7 @@ def convert_params_from_subspace_to_full(params_subspace, projection_matrix, par
         The projection matrix
     params_full_init: jnp.ndarray(D)
         The initial full model weights
-    
+
     Returns
     -------
     params_full: jnp.ndarray(D)
@@ -98,7 +96,6 @@ def projected_loss(params_subspace, batch, projection_matrix, params_full_init, 
     1. Project theta_subspace ∈ R^d => theta ∈ R^D
     2. Reconstruct the pytree of the reconstructed weights
     3. Compute loss of the model w.r.t. theta_subspace
-
     Parameters
     ----------
     theta_subspace: jnp.ndarray(d)
@@ -110,8 +107,8 @@ def projected_loss(params_subspace, batch, projection_matrix, params_full_init, 
     params_full_init: jnp.ndarray(D)
         The initial full model weights
     flat_to_pytree_fn: function
-        The reconstruction function from array(D) to pytree 
-    
+        The reconstruction function from array(D) to pytree
+
     Returns
     -------
     loss: float
@@ -134,7 +131,7 @@ def adam_update(grads, params, mass, velocity, hyperparams):
 
 
 def generate_random_basis(key, d, D):
-    projection_matrix = random.normal(key, shape=(d,D))
+    projection_matrix = random.normal(key, shape=(d, D))
     projection_matrix = projection_matrix / jnp.linalg.norm(projection_matrix, axis=-1, keepdims=True)
     return projection_matrix
 
@@ -154,11 +151,8 @@ def subspace_learning(key, model, datasets, d, hyperparams, n_epochs=300):
                                      params_full_init=params_full_init)
     loss_grad_wrt_params_subspace = jax.grad(projected_loss_partial)
 
-    params_subspace = jnp.zeros((1, d))
-    mass = jnp.zeros((1, d))
-    velocity = jnp.zeros((1, d))
-
-    for e in range(n_epochs):
+    def train_step(params, i):
+        params_subspace, mass, velocity = params
         grads = loss_grad_wrt_params_subspace(params_subspace, datasets["train"])
         params_subspace, mass, velocity = adam_update(grads, params_subspace, mass, velocity, hyperparams)
 
@@ -167,64 +161,82 @@ def subspace_learning(key, model, datasets, d, hyperparams, n_epochs=300):
 
         epoch_loss = cross_entropy_loss(params_pytree, datasets["train"])
         epoch_accuracy = normal_accuracy(params_pytree, datasets["train"])
-        if e % 100 == 0 or e == n_epochs - 1:
-            end = "\n"
-            epoch_val_accuracy = normal_accuracy(params_pytree, datasets["test"])
-            val_str = f" || val acc: {epoch_val_accuracy:0.2%}"
-        else:
-            end = "\r"
-            val_str = ""
-            
-        metric_str = f"epoch: {e+1:03} || acc: {epoch_accuracy:0.2%} || loss:{epoch_loss:0.2f}"
-        metric_str += val_str
-        print(metric_str, end=end)
+        epoch_val_accuracy = normal_accuracy(params_pytree, datasets["test"])
+        return (params_subspace, mass, velocity), (epoch_loss, epoch_accuracy, epoch_val_accuracy)
 
-    return params_subspace, epoch_val_accuracy
+    initial_params_subspace = jnp.zeros((1, d))
+    mass = jnp.zeros((1, d))
+    velocity = jnp.zeros((1, d))
+    epochs = jnp.arange(n_epochs)
 
+    (params_subspace, _, _), (loss_values, train_accuracies, val_accuracies) = jax.lax.scan(train_step, (
+    initial_params_subspace, mass, velocity), epochs)
+
+    return params_subspace, loss_values, train_accuracies, val_accuracies
 
 
-plt.rcParams["axes.spines.right"] = False
-plt.rcParams["axes.spines.top"] = False
+def print_metrics(loss_values, train_accuracies, val_accuracies, print_every=100):
+    n_epochs = loss_values.size
 
-key = random.PRNGKey(314)
-train_ds, test_ds = get_datasets()
-_, *n_features = train_ds["X"].shape
-n_features = np.prod(n_features)
-train_ds["X"] = train_ds["X"].reshape(-1, n_features)
-test_ds["X"] = test_ds["X"].reshape(-1, n_features)
+    metric_str = lambda e, epoch_loss, epoch_train_accuracy, epoch_val_accuracy: \
+        "epoch: {:03} || loss:{:.2f} || acc: {:.2%} || val acc: {:.2%}".format(e, epoch_loss, epoch_train_accuracy,
+                                                                               epoch_val_accuracy)
 
-datasets = {
-    "train": train_ds,
-    "test": test_ds,
-}
+    for e in range(0, n_epochs, print_every):
+        epoch_loss, epoch_train_accuracy, epoch_val_accuracy = loss_values[e], train_accuracies[e], val_accuracies[e]
+        print(metric_str(e + 1, epoch_loss, epoch_train_accuracy, epoch_val_accuracy))
 
-hyperparams = {
-    "lr": 1e-1,
-    "beta_1": 0.9,
-    "beta_2": 0.999,
-    "epsilon": 1e-7
-}
+    if e != n_epochs - 1:
+        epoch_loss, epoch_train_accuracy, epoch_val_accuracy = loss_values[n_epochs - 1], train_accuracies[
+            n_epochs - 1], val_accuracies[n_epochs - 1]
+        print(metric_str(n_epochs, epoch_loss, epoch_train_accuracy, epoch_val_accuracy))
 
-min_dim, max_dim = 10, 1300
-jump_size = 50
-subspace_dims = [2] + list(range(min_dim, max_dim, jump_size))
 
-acc_vals = []
-n_epochs = 100 #300
-for dim in subspace_dims:
-    init_time = time()
-    print(f"\nTesting subpace {dim}")
-    params_subspace, accuracy = subspace_learning(key, MLP, datasets, dim, hyperparams, n_epochs=n_epochs)
-    end_time = time()
-    print(f"Running time: {end_time - init_time:0.2f}s")
-    acc_vals.append(accuracy)
+if __name__ == "__main__":
+    plt.rcParams["axes.spines.right"] = False
+    plt.rcParams["axes.spines.top"] = False
 
-fig, ax = plt.subplots(figsize=(6, 3))
-plt.plot(subspace_dims[::2], acc_vals[::2], marker="o")
-plt.yticks([0.2, 0.4, 0.6, 0.8, 1.0])
-plt.axhline(y=0.9, c="tab:gray", linestyle="--")
-plt.xlabel("Subspace dim $d$", fontsize=13)
-plt.ylabel("Validation accuracy", fontsize=13)
-plt.tight_layout()
-pml.savefig("subspace_learning.pdf")
-plt.show()
+    key = random.PRNGKey(314)
+    train_ds, test_ds = get_datasets()
+    _, *n_features = train_ds["X"].shape
+    n_features = np.prod(n_features)
+    train_ds["X"] = train_ds["X"].reshape(-1, n_features)
+    test_ds["X"] = test_ds["X"].reshape(-1, n_features)
+
+    datasets = {
+        "train": train_ds,
+        "test": test_ds,
+    }
+
+    hyperparams = {
+        "lr": 1e-2,
+        "beta_1": 0.9,
+        "beta_2": 0.999,
+        "epsilon": 1e-7
+    }
+
+    min_dim, max_dim = 10, 1300
+    jump_size = 50
+    subspace_dims = [2] + list(range(min_dim, max_dim, jump_size))
+
+    acc_vals = []
+    n_epochs = 300
+
+    for dim in subspace_dims:
+        init_time = time()
+        print(f"\nTesting subpace {dim}")
+        params_subspace, loss_values, train_accuracies, val_accuracies = subspace_learning(key, MLP, datasets, dim,
+                                                                                           hyperparams, n_epochs=n_epochs)
+        end_time = time()
+        print(f"Running time: {end_time - init_time:0.2f}s")
+        acc_vals.append(val_accuracies[-1])
+        print_metrics(loss_values, train_accuracies, val_accuracies, print_every=100)
+
+    fig, ax = plt.subplots(figsize=(6, 3))
+    plt.plot(subspace_dims[::2], acc_vals[::2], marker="o")
+    plt.yticks([0.2, 0.4, 0.6, 0.8, 1.0])
+    plt.axhline(y=0.9, c="tab:gray", linestyle="--")
+    plt.xlabel("Subspace dim $d$", fontsize=13)
+    plt.ylabel("Validation accuracy", fontsize=13)
+    plt.tight_layout()
+    pml.savefig("subspace_learning.pdf")
