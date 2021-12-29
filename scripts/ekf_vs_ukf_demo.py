871c6c5079503824e5501c73d42d4fed51b546d3
Gerardo Durán Martín <g.duran@me.com>
2021-12-29 14:00:17 +0000

    refactor: ekf_vs_ukf_demo

--- scripts/ekf_vs_ukf_demo.py
+++ scripts/ekf_vs_ukf_demo.py
@@ -1,12 +1,13 @@
 # Compare extended Kalman filter with unscented kalman filter on a nonlinear 3d tracking problem
-# Author: Gerardo Duran-Martin (@gerdm)
+# Author: Gerardo Durán-Martín (@gerdm)
 
 # !pip install git+git://github.com/probml/jsl
 
 from jsl.demos import ekf_vs_ukf
-import pyprobml_utils as pml
+import matplotlib.pyplot as plt
 
 figures = ekf_vs_ukf.main()
 for name, figure in figures.items():
-    figure.show()
-    pml.savefig(f"{name}.pdf")
+    filename = f"./../figures/{name}.pdf"
+    figure.savefig(filename)
+plt.show()
