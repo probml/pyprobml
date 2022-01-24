388eb37544676bf8affee548a3de0c218ad1fd24
Kevin P Murphy <murphyk@gmail.com>
2022-01-23 21:36:52 -0800

    MTM

---
+++ scripts/.idea/hello_world.py
@@ -0,0 +1,5 @@
+def test():
+    print("hello world")
+
+if __name__ == "__main__":
+    test()
---
+++ scripts/hello_world.py
@@ -0,0 +1,8 @@
+
+
+def test():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    test()
