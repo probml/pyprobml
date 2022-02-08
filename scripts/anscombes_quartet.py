e07b84aa1616eb54fd26d71efb22f8fcff6edba8
Kevin P Murphy <murphyk@gmail.com>
2022-02-07 17:33:00 -0800

    compare variance estimators for anscombe

--- scripts/anscombes_quartet.py
+++ scripts/anscombes_quartet.py
@@ -23,11 +23,25 @@ for name in names:
     ax = plt.gca()
     ax.set_xlim(0, 20)
     ax.set_ylim(0, 14)
-    mx = df2['x'].to_numpy().mean(); my = df2['y'].to_numpy().mean()
+    mx = df2['x'].to_numpy().mean();
+    my = df2['y'].to_numpy().mean()
     ax.set_title(f'{name}, mx={mx:0.3f}, my={my:0.3f}', fontsize=12)
     print(df2[['x', 'y']].agg(['count', 'mean', 'var']))
     pml.savefig(f"anscombes_quartet_{name}.pdf")
 
 
+# Compare the two different estimators for the variance
+# https://github.com/probml/pml-book/issues/264
+for d in ['I', 'II', 'III', 'IV']:
+    print('dataset ', d)
 
+    x = df[df['dataset'] == d]['x'].to_numpy()
+    print('var x, MLE = {:.2f}'.format(((x - x.mean()) ** 2).mean()))
+    print('var x, numpy: {:.2f}'.format(x.var()))
+    print('var x, unbiased estimator: {:.2f}\n'.format(x.var(ddof=1)))
+
+    y = df[df['dataset'] == d]['y'].to_numpy()
+    print('var y, MLE = {:.2f}'.format(((y - y.mean())**2).mean()))
+    print('var y, numpy: {:.2f}'.format(y.var()))
+    print('var y, unbiased estimator: {:.2f}\n'.format(y.var(ddof=1)))
 
