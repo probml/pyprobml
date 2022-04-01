d3056a122a3ca1aa07b6ecdd2e3f166475744dc1
benlau6 <benyplau@gmail.com>
2022-04-02 02:20:31 +0800

    refactor naming, line breaks, function def

--- scripts/exact_1d_bayesian_filtering_example3.py
+++ scripts/exact_1d_bayesian_filtering_example3.py
@@ -9,32 +9,40 @@ import exact_1d_bayesian_filtering_utils as utils
 
 
 # state transition function
-def f(x, v, k=None):
+def state_trans_func3(x, v, k=None):
     return 0.7 * x + v
 
 
 # measurement function
-def sat(x, minval, maxval):
+def saturate(x, minval, maxval):
     return jnp.maximum(jnp.minimum(x, maxval), minval)
 
 
-def h(x, e):
-    return sat(x + e, minval=-1.5, maxval=1.5)
+def measure_func3(x, e, minval=-1.5, maxval=1.5):
+    return saturate(x + e, minval=minval, maxval=maxval)
 
 
 # to get x from measurement without noise
-def inv_h(y):
+def inv_measure_func3(y):
     return y
 
 
 # functions to get sample
-v_rvs = lambda rng_key, shape: random.normal(rng_key, shape=shape)
-e_rvs = lambda rng_key, shape: random.normal(rng_key, shape=shape) * jnp.sqrt(0.5)
-x0_rvs = lambda rng_key, shape: random.normal(rng_key, shape=shape) * jnp.sqrt(0.1)
+def v_rvs3(rng_key, shape):
+    return random.normal(rng_key, shape=shape)
+
+
+def e_rvs3(rng_key, shape):
+    return random.normal(rng_key, shape=shape) * jnp.sqrt(0.5)
+
+
+def x0_rvs3(rng_key, shape):
+    return random.normal(rng_key, shape=shape) * jnp.sqrt(0.1)
 
 
 # functions to get density
-x0_pdf = functools.partial(jsp.stats.norm.pdf, scale=jnp.sqrt(0.1))
+x0_pdf3 = functools.partial(jsp.stats.norm.pdf, scale=jnp.sqrt(0.1))
+
 
 # input arguments
 rng_key = random.PRNGKey(0)
@@ -42,38 +50,40 @@ num_samples = 10000
 grid_minval = -6
 grid_maxval = 6
 num_grid_points = 500
-K = 24
-k = 18
+max_iter = 24
+iter_ = 18
+plot_all_densities = False
 
-key, subkey = random.split(rng_key, num=2)
+rng_key, subkey = random.split(rng_key, num=2)
 x_grid, x_true, y = utils.experiment_setup(
-    rng_key=key,
-    grid_minval=grid_minval,
-    grid_maxval=grid_maxval,
-    num_grid_points=num_grid_points,
-    x0_rvs=x0_rvs,
-    v_rvs=v_rvs,
-    e_rvs=e_rvs,
-    f=f,
-    h=h,
-    K=K,
-    plot_xy=True,
+    rng_key=rng_key, grid_minval=grid_minval, grid_maxval=grid_maxval,
+    num_grid_points=num_grid_points, x0_rvs=x0_rvs3, v_rvs=v_rvs3,
+    e_rvs=e_rvs3, f=state_trans_func3, h=measure_func3,
+    max_iter=max_iter, plot_xy=True,
 )
-utils.novel_experiment(
-    rng_key=subkey,
-    x_grid=x_grid,
-    x_true=x_true,
-    y=y,
-    x0_pdf=x0_pdf,
-    v_rvs=v_rvs,
-    e_rvs=e_rvs,
-    f=f,
-    h=h,
-    inv_h=inv_h,
-    K=K,
-    k=k,
-    num_samples=num_samples,
-    plot_all_densities=False,
-    title=f"Saturated measurements example densities at $x_{{{k}}}$",
+
+p_filter, p_pred = utils.novel_density(
+    subkey, y, x_grid,
+    x0_pdf3, v_rvs3, e_rvs3,
+    state_trans_func3, measure_func3, num_samples,
+    max_iter, kernel_variance=0.15,
 )
+p_smooth = None
+
+if plot_all_densities:
+    # looking for weird density plot by plotting all max_iter densities
+    utils.plot_densities(
+        x_true, y, inv_measure_func3,
+        x_grid, p_pred, p_filter,
+        p_smooth, max_iter,
+    )
+
+# plot the kth density
+utils.plot_density(
+    x_true, y, inv_measure_func3,
+    x_grid, p_pred, p_filter,
+    p_smooth, k=iter_, legend=True,
+    ax=None, title=f"Saturated measurements example densities at $x_{{{iter_}}}$",
+)
+
 plt.show()
--- scripts/exact_1d_bayesian_filtering_utils.py
+++ scripts/exact_1d_bayesian_filtering_utils.py
@@ -1,3 +1,16 @@
+"""
+This script implements algorithms and examples from "Computation and visualization of posterior densities
+in scalar nonlinear and non-Gaussian Bayesian filtering and smoothing problems",
+which was written by Michael Roth and Fredrik Gustafsson
+
+Reference:
+https://liu.diva-portal.org/smash/get/diva2:1173608/FULLTEXT02.pdf
+Part of: 2017 IEEE INTERNATIONAL CONFERENCE ON ACOUSTICS, SPEECH AND SIGNAL PROCESSING (ICASSP),
+2017, pp. 4686-4690. ISBN: 978-1-5090- 4117-6
+Book Series: International Conference on Acoustics Speech and Signal Processing ICASSP, 1520-6149.
+"""
+
+
 from typing import Callable
 import functools
 
@@ -11,18 +24,22 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({"font.size": 16})
 
 
-def generate_x_true(rng_key, K, x0_rvs: Callable, v_rvs: Callable, f: Callable):
+def generate_x_true(
+        rng_key: jnp.DeviceArray, max_iter: int, x0_rvs: Callable,
+        v_rvs: Callable, f: Callable):
     rng_keys = random.split(rng_key, num=2)
     x0 = x0_rvs(rng_keys[0], shape=())
-    x_true = jnp.zeros(shape=(K + 1,))
+    x_true = jnp.zeros(shape=(max_iter + 1,))
     x_true = x_true.at[0].set(x0)
     v = v_rvs(rng_keys[1], shape=x_true.shape)
-    for k in range(1, K + 1):
+    for k in range(1, max_iter + 1):
         x_true = x_true.at[k].set(f(x_true[k - 1], v[k - 1], k=k - 1))
     return jnp.array(x_true)
 
 
-def generate_y(rng_key, x_true, e_rvs: Callable, h: Callable):
+def generate_y(
+        rng_key: jnp.DeviceArray, x_true: jnp.DeviceArray, e_rvs: Callable,
+        h: Callable):
     shape = x_true.shape
     e = e_rvs(rng_key, shape=shape)
     y = h(x_true, e)
@@ -60,40 +77,31 @@ def inversion_sampling(rng_key, x_grid, px_grid, num_samples):
 
 def kde(x_grid, x, kernel_variance):
     delta = x_grid[1] - x_grid[0]
-
-    px = jax.vmap(functools.partial(jsp.stats.norm.pdf, loc=x, scale=kernel_variance))(
-        x_grid
-    )
+    # broadcast it into (n_x_grid, nx)
+    x_grid = jnp.tile(x_grid[..., jnp.newaxis], (1, x.shape[0]))
+    px = jsp.stats.norm.pdf(x_grid, loc=x, scale=kernel_variance)
     px = jnp.sum(px, axis=1)
     px = px / jnp.sum(px) / delta
     return px
 
 
 def novel_density(
-    rng_key,
-    y,
-    x_grid,
-    x0_pdf,
-    v_rvs: Callable,
-    e_rvs: Callable,
-    f: Callable,
-    h: Callable,
-    num_samples: int,
-    K,
-    kernel_variance,
-):
+        rng_key: jnp.DeviceArray, y: jnp.DeviceArray, x_grid: jnp.DeviceArray,
+        x0_pdf: Callable, v_rvs: Callable, e_rvs: Callable,
+        f: Callable, h: Callable, num_samples: int,
+        max_iter: int, kernel_variance: float):
     num_grid_points = x_grid.shape[0]
     delta = x_grid[1] - x_grid[0]
 
-    p_filter = jnp.zeros(shape=(K + 1, num_grid_points))
+    p_filter = jnp.zeros(shape=(max_iter + 1, num_grid_points))
     p_filter = p_filter.at[0, :].set(x0_pdf(x_grid))
     p_filter = p_filter.at[0, :].set(p_filter[0, :] / jnp.sum(p_filter[0, :]))
-    p_pred = jnp.ones(shape=(K + 1, num_grid_points)) * jnp.inf
+    p_pred = jnp.ones(shape=(max_iter + 1, num_grid_points)) * jnp.inf
     rng_keys = random.split(rng_key, num=3)
-    v = v_rvs(rng_keys[0], shape=(K + 1, num_samples))
-    e = e_rvs(rng_keys[1], shape=(K + 1, num_samples))
+    v = v_rvs(rng_keys[0], shape=(max_iter + 1, num_samples))
+    e = e_rvs(rng_keys[1], shape=(max_iter + 1, num_samples))
 
-    for k in range(1, K + 1):
+    for k in range(1, max_iter + 1):
         x = inversion_sampling(
             rng_keys[2],
             x_grid,
@@ -129,26 +137,20 @@ def novel_density(
 
 
 def point_mass_density(
-    y,
-    x_grid,
-    x0_pdf,
-    x_pdf: Callable,
-    v_pdf: Callable,
-    e_pdf: Callable,
-    f: Callable,
-    h: Callable,
-):
+        y: jnp.DeviceArray, x_grid: jnp.DeviceArray, x0_pdf: Callable,
+        x_pdf: Callable, v_pdf: Callable, e_pdf: Callable,
+        f: Callable, h: Callable):
     num_grid_points = x_grid.shape[0]
-    K = len(y) - 1
+    max_iter = len(y) - 1
     delta = x_grid[1] - x_grid[0]
     X = jnp.tile(x_grid, (num_grid_points, 1))
 
-    p_filter = jnp.zeros(shape=(K + 1, num_grid_points))
+    p_filter = jnp.zeros(shape=(max_iter + 1, num_grid_points))
     p_filter = p_filter.at[0, :].set(x0_pdf(x_grid))
     p_filter = p_filter.at[0, :].set(p_filter[0, :] / sum(p_filter[0, :]))  # normalize
-    p_pred = jnp.ones(shape=(K + 1, num_grid_points)) * jnp.inf
+    p_pred = jnp.ones(shape=(max_iter + 1, num_grid_points)) * jnp.inf
 
-    for k in range(1, K + 1):
+    for k in range(1, max_iter + 1):
         # p(xk, xk-1 | y(1:k-1))
         px = x_pdf(k=k - 1, x_new=X.T, x=X, v_pdf=v_pdf, f=f)
         p_joint = px * p_filter[k - 1, :]
@@ -164,7 +166,7 @@ def point_mass_density(
         p_filter = p_filter.at[k, :].set(p_filter[k, :] / jnp.sum(p_filter[k, :]))
 
     p_smooth = jnp.array(p_filter.copy())
-    for k in range(K - 1, -1, -1):
+    for k in range(max_iter - 1, -1, -1):
         # p(xk, xk-1 | y(1:k-1))
         px = x_pdf(k=k, x_new=X.T, x=X, v_pdf=v_pdf, f=f)
         px = px * p_smooth[k + 1, :].T / p_pred[k + 1, :].T
@@ -179,20 +181,11 @@ def point_mass_density(
 
 
 def plot_density(
-    x_true,
-    y,
-    inv_h,
-    x_grid,
-    p_pred,
-    p_filter,
-    p_smooth=None,
-    k=1,
-    legend=True,
-    ax=None,
-    vfill=None,
-    title="",
-    linewidth=4.5,
-):
+        x_true, y, inv_h,
+        x_grid, p_pred, p_filter,
+        p_smooth=None, k=1, legend=True,
+        ax=None, vfill=None, title="",
+        linewidth=4.5):
     if ax is None:
         fig, ax = plt.subplots(figsize=(12, 8))
 
@@ -226,52 +219,38 @@ def plot_density(
 
 
 def plot_densities(
-    x_true, y, inv_h, x_grid, p_pred, p_filter, p_smooth, K, legend=True
-):
-    nplot = K + 1
+        x_true, y, inv_h, x_grid, p_pred, p_filter, p_smooth, max_iter, legend=True):
+    nplot = max_iter + 1
     ncol = 10
     nrow = nplot // ncol + 1
     fig, axes = plt.subplots(nrow, ncol, figsize=(12, 6), sharex=True, sharey=True)
     axes = axes.ravel()
 
-    plt.suptitle("All K density plots to look for weird pattern")
+    plt.suptitle("All density plots to look for weird pattern")
 
-    for k in range(1, K + 1):
+    for k in range(1, max_iter + 1):
         plot_density(
-            x_true,
-            y,
-            inv_h,
-            x_grid=x_grid,
-            p_pred=p_pred,
-            p_filter=p_filter,
-            p_smooth=p_smooth,
-            k=k,
-            ax=axes[k],
-            legend=False,
+            x_true, y, inv_h, x_grid=x_grid,
+            p_pred=p_pred, p_filter=p_filter, p_smooth=p_smooth,
+            k=k, ax=axes[k], legend=False,
             linewidth=1.5,
         )
     fig.tight_layout()
 
 
 def experiment_setup(
-    rng_key,
-    grid_minval,
-    grid_maxval,
-    num_grid_points,
-    x0_rvs,
-    v_rvs,
-    e_rvs,
-    f,
-    h,
-    K,
-    plot_xy=False,
-):
+    rng_key, grid_minval, grid_maxval,
+    num_grid_points, x0_rvs, v_rvs,
+    e_rvs, f, h,
+    max_iter, plot_xy=False):
     # create 1d grid
     x_grid = jnp.linspace(grid_minval, grid_maxval, num_grid_points)
 
     # generate true states
     rng_key, rng_subkey = random.split(rng_key)
-    x_true = generate_x_true(rng_subkey, K=K, x0_rvs=x0_rvs, v_rvs=v_rvs, f=f)
+    x_true = generate_x_true(
+        rng_subkey, max_iter=max_iter, x0_rvs=x0_rvs, v_rvs=v_rvs, f=f
+    )
 
     # generate measurement
     rng_key, rng_subkey = random.split(rng_key)
@@ -281,104 +260,10 @@ def experiment_setup(
         # plot trajectory and the measurement
         fig, ax = plt.subplots(figsize=(12, 8))
         ax.set_title("Trajectory and Measurement versus k")
-        ax.plot(range(K + 1), x_true, label="True state", color="k")
-        ax.plot(range(K + 1), y, label="Measurements", color="r")
+        ax.plot(range(max_iter + 1), x_true, label="True state", color="k")
+        ax.plot(range(max_iter + 1), y, label="Measurements", color="r")
         ax.set_ylabel("$x_k, y_k$")
         ax.set_xlabel("k")
         ax.legend(prop={"size": 16})
 
     return x_grid, x_true, y
-
-
-def point_mass_experiment(
-    x_grid,
-    x_true,
-    y,
-    x0_pdf,
-    x_pdf,
-    v_pdf,
-    e_pdf,
-    f,
-    h,
-    inv_h,
-    K,
-    k,
-    plot_all_densities=False,
-    title="",
-):
-    p_filter, p_pred, p_smooth = point_mass_density(
-        y, x_grid, x0_pdf, x_pdf=x_pdf, v_pdf=v_pdf, e_pdf=e_pdf, f=f, h=h
-    )
-
-    if plot_all_densities:
-        # looking for weird density plot by plotting all K densities
-        plot_densities(x_true, y, inv_h, x_grid, p_pred, p_filter, p_smooth, K)
-
-    # plot the kth density
-    plot_density(
-        x_true,
-        y,
-        inv_h,
-        x_grid,
-        p_pred,
-        p_filter,
-        p_smooth,
-        k=k,
-        legend=True,
-        ax=None,
-        title=title,
-    )
-
-
-def novel_experiment(
-    rng_key,
-    x_grid,
-    x_true,
-    y,
-    v_rvs,
-    e_rvs,
-    x0_pdf,
-    f,
-    h,
-    inv_h,
-    K,
-    k,
-    num_samples,
-    plot_all_densities=False,
-    title="",
-):
-    rng_key, rng_subkey = random.split(rng_key)
-    p_filter, p_pred = novel_density(
-        rng_subkey,
-        y,
-        x_grid,
-        x0_pdf,
-        v_rvs,
-        e_rvs,
-        f,
-        h,
-        num_samples,
-        K,
-        kernel_variance=0.15,
-    )
-    p_smooth = None
-
-    if plot_all_densities:
-        # looking for weird density plot by plotting all K densities
-        plot_densities(x_true, y, inv_h, x_grid, p_pred, p_filter, p_smooth, K)
-
-    # plot the kth density
-    plot_density(
-        x_true,
-        y,
-        inv_h,
-        x_grid,
-        p_pred,
-        p_filter,
-        p_smooth,
-        vfill=(-1.5, 1.5),
-        k=k,
-        legend=True,
-        ax=None,
-        title=title,
-    )
