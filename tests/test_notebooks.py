c652417c0c7e52ab45ee1ed09e415f5fadebaeec
patel-zeel <patel_zeel@iitgn.ac.in>
2022-04-03 16:03:24 +0530

    disable `NO_SAVE_FIGS` till workflow gets heavy

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,7 +4,7 @@ import subprocess
 from glob import glob
 
 os.environ["LATEXIFY"] = ""  # To enable latexify code
-os.environ["NO_SAVE_FIGS"] = ""  # To avoid saving the figures
+# os.environ["NO_SAVE_FIGS"] = ""  # To avoid saving the figures
 notebooks1 = glob("notebooks/book1/*.ipynb")
 notebooks2 = glob("notebooks/book2/*.ipynb")
 notebooks = notebooks1 + notebooks2
