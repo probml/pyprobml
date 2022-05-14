f9119fe8a2592c058197ac642104603f5d418461
karm-patel <karmpatel216@gmail.com>
2022-05-14 19:35:58 +0530

    Add local username in `combine_results`

--- .github/scripts/combine_results.py
+++ .github/scripts/combine_results.py
@@ -1,6 +1,11 @@
 import os
 import re
+import argparse
+parser = argparse.ArgumentParser(description="delete firestore")
+parser.add_argument("-user_name", "--user_name", type=str, help="")
+args = parser.parse_args()
 
+user_name = args.user_name
 # Read number of jobs
 with open(".github/workflows/notebooks.yml") as f:
     runners = re.findall("runner_id: (.*)", f.read())
@@ -13,7 +18,7 @@ for folder in FOLDERS:
         os.system("rm -rf " + folder)
     os.makedirs(folder)
 
-repo = "https://github.com/probml/pyprobml.git"
+repo = f"https://github.com/{user_name}/pyprobml.git"
 for job in range(n_jobs):
     for folder in FOLDERS:
         branch = f"{folder}_{job}"
