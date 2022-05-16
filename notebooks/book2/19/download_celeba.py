5ac5e19bb0b5f5678f6b3f2daef19a64db27d5b0
karm-patel <karmpatel216@gmail.com>
2022-05-16 15:30:34 +0530

    black configured

--- notebooks/book2/19/download_celeba.py
+++ notebooks/book2/19/download_celeba.py
@@ -1,10 +1,8 @@
-
-
 # The CelebA dataloader is from
 # https://github.com/sayantanauddy/vae_lightning/blob/main/data.py
 # and extracts the data from kaggle.
-#First make sure you have kaggle.json,
-#as explained at https://github.com/Kaggle/kaggle-api#api-credentials.
+# First make sure you have kaggle.json,
+# as explained at https://github.com/Kaggle/kaggle-api#api-credentials.
 
 # We create a dataloader with the required image size,
 # and thus force the code to first download the data locally.
@@ -26,22 +24,14 @@ from torchvision.datasets.utils import verify_str_arg
 
 import pytorch_lightning as pl
 
-#from celeba_data import  CelebADataModule
+# from celeba_data import  CelebADataModule
 
 
 class CelebADataset(Dataset):
     """CelebA Dataset class"""
 
-    def __init__(self,
-                 root,
-                 split="train",
-                 target_type="attr",
-                 transform=None,
-                 target_transform=None,
-                 download=False
-                 ):
-        """
-        """
+    def __init__(self, root, split="train", target_type="attr", transform=None, target_transform=None, download=False):
+        """ """
 
         self.root = root
         self.split = split
@@ -55,7 +45,7 @@ class CelebADataset(Dataset):
             self.target_type = [target_type]
 
         if not self.target_type and self.target_transform is not None:
-            raise RuntimeError('target_transform is specified but target_type is empty')
+            raise RuntimeError("target_transform is specified but target_type is empty")
 
         if download:
             self.download_from_kaggle()
@@ -74,11 +64,12 @@ class CelebADataset(Dataset):
         # This file is not available in Kaggle
         # identity = pd.read_csv(fn("identity_CelebA.csv"), delim_whitespace=True, header=None, index_col=0)
         bbox = pd.read_csv(fn("list_bbox_celeba.csv"), delim_whitespace=False, header=0, index_col=0)
-        landmarks_align = pd.read_csv(fn("list_landmarks_align_celeba.csv"), delim_whitespace=False, header=0,
-                                      index_col=0)
+        landmarks_align = pd.read_csv(
+            fn("list_landmarks_align_celeba.csv"), delim_whitespace=False, header=0, index_col=0
+        )
         attr = pd.read_csv(fn("list_attr_celeba.csv"), delim_whitespace=False, header=0, index_col=0)
 
-        mask = slice(None) if split_ is None else (splits['partition'] == split_)
+        mask = slice(None) if split_ is None else (splits["partition"] == split_)
 
         self.filename = splits[mask].index.values
         # self.identity = torch.as_tensor(identity[mask].values)
@@ -91,8 +82,12 @@ class CelebADataset(Dataset):
     def download_from_kaggle(self):
 
         # Annotation files will be downloaded at the end
-        label_files = ['list_attr_celeba.csv', 'list_bbox_celeba.csv', 'list_eval_partition.csv',
-                       'list_landmarks_align_celeba.csv']
+        label_files = [
+            "list_attr_celeba.csv",
+            "list_bbox_celeba.csv",
+            "list_eval_partition.csv",
+            "list_landmarks_align_celeba.csv",
+        ]
 
         # Check if files have been downloaded already
         files_exist = False
@@ -112,25 +107,26 @@ class CelebADataset(Dataset):
             # Kaggle authentication
             # Remember to place the API token from Kaggle in $HOME/.kaggle
             from kaggle.api.kaggle_api_extended import KaggleApi
+
             api = KaggleApi()
             api.authenticate()
 
             # Download all files of a dataset
             # Signature: dataset_download_files(dataset, path=None, force=False, quiet=True, unzip=False)
-            api.dataset_download_files(dataset='jessicali9530/celeba-dataset',
-                                       path=self.root,
-                                       unzip=True,
-                                       force=False,
-                                       quiet=False)
+            api.dataset_download_files(
+                dataset="jessicali9530/celeba-dataset", path=self.root, unzip=True, force=False, quiet=False
+            )
 
             # Downoad the label files
             # Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
             for label_file in label_files:
-                api.dataset_download_file(dataset='jessicali9530/celeba-dataset',
-                                          file_name=label_file,
-                                          path=self.root,
-                                          force=False,
-                                          quiet=False)
+                api.dataset_download_file(
+                    dataset="jessicali9530/celeba-dataset",
+                    file_name=label_file,
+                    path=self.root,
+                    force=False,
+                    quiet=False,
+                )
 
             # Clear any remaining *.csv.zip files
             files_to_delete = glob.glob(os.path.join(self.root, "*.csv.zip"))
@@ -140,10 +136,7 @@ class CelebADataset(Dataset):
             print("Done!")
 
     def __getitem__(self, index: int):
-        X = PIL.Image.open(os.path.join(self.root,
-                                        "img_align_celeba",
-                                        "img_align_celeba",
-                                        self.filename[index]))
+        X = PIL.Image.open(os.path.join(self.root, "img_align_celeba", "img_align_celeba", self.filename[index]))
 
         target = []
         for t in self.target_type:
@@ -176,16 +169,17 @@ class CelebADataset(Dataset):
 
 
 class CelebADataModule(pl.LightningDataModule):
-
-    def __init__(self,
-                 data_dir,
-                 target_type="attr",
-                 train_transform=None,
-                 val_transform=None,
-                 target_transform=None,
-                 download=False,
-                 batch_size=32,
-                 num_workers=8):
+    def __init__(
+        self,
+        data_dir,
+        target_type="attr",
+        train_transform=None,
+        val_transform=None,
+        target_transform=None,
+        download=False,
+        batch_size=32,
+        num_workers=8,
+    ):
         super().__init__()
 
         self.data_dir = data_dir
@@ -200,65 +194,65 @@ class CelebADataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         # Training dataset
-        self.celebA_trainset = CelebADataset(root=self.data_dir,
-                                             split='train',
-                                             target_type=self.target_type,
-                                             download=self.download,
-                                             transform=self.train_transform,
-                                             target_transform=self.target_transform)
+        self.celebA_trainset = CelebADataset(
+            root=self.data_dir,
+            split="train",
+            target_type=self.target_type,
+            download=self.download,
+            transform=self.train_transform,
+            target_transform=self.target_transform,
+        )
 
         # Validation dataset
-        self.celebA_valset = CelebADataset(root=self.data_dir,
-                                           split='valid',
-                                           target_type=self.target_type,
-                                           download=False,
-                                           transform=self.val_transform,
-                                           target_transform=self.target_transform)
+        self.celebA_valset = CelebADataset(
+            root=self.data_dir,
+            split="valid",
+            target_type=self.target_type,
+            download=False,
+            transform=self.val_transform,
+            target_transform=self.target_transform,
+        )
 
         # Test dataset
-        self.celebA_testset = CelebADataset(root=self.data_dir,
-                                            split='test',
-                                            target_type=self.target_type,
-                                            download=False,
-                                            transform=self.val_transform,
-                                            target_transform=self.target_transform)
+        self.celebA_testset = CelebADataset(
+            root=self.data_dir,
+            split="test",
+            target_type=self.target_type,
+            download=False,
+            transform=self.val_transform,
+            target_transform=self.target_transform,
+        )
 
     def train_dataloader(self):
-        return DataLoader(self.celebA_trainset, batch_size=self.batch_size, shuffle=True, drop_last=True,
-                          num_workers=self.num_workers)
+        return DataLoader(
+            self.celebA_trainset, batch_size=self.batch_size, shuffle=True, drop_last=True, num_workers=self.num_workers
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.celebA_valset, batch_size=self.batch_size, shuffle=False, drop_last=False,
-                          num_workers=self.num_workers)
+        return DataLoader(
+            self.celebA_valset, batch_size=self.batch_size, shuffle=False, drop_last=False, num_workers=self.num_workers
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.celebA_testset, batch_size=self.batch_size, shuffle=False, drop_last=False,
-                          num_workers=self.num_workers)
-
-
+        return DataLoader(
+            self.celebA_testset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=self.num_workers,
+        )
 
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_float(
-    'crop_size', default=128,
-    help=('The dataset we are interested to train out vae on')
-)
+flags.DEFINE_float("crop_size", default=128, help=("The dataset we are interested to train out vae on"))
 
-flags.DEFINE_integer(
-    'batch_size', default=256,
-    help=('Batch size for training.')
-)
+flags.DEFINE_integer("batch_size", default=256, help=("Batch size for training."))
 
-flags.DEFINE_integer(
-    'image_size', default=64,
-    help=('Image size for training.')
-)
+flags.DEFINE_integer("image_size", default=64, help=("Image size for training."))
 
-flags.DEFINE_string(
-    'data_dir', default="kaggle",
-    help=('Data directory for training.')
-)
+flags.DEFINE_string("data_dir", default="kaggle", help=("Data directory for training."))
+
 
 def celeba_dataloader(bs, IMAGE_SIZE, CROP, DATA_PATH):
     trans = []
@@ -269,12 +263,14 @@ def celeba_dataloader(bs, IMAGE_SIZE, CROP, DATA_PATH):
     trans.append(transforms.ToTensor())
     transform = transforms.Compose(trans)
 
-    dm = CelebADataModule(data_dir=DATA_PATH,
-                                target_type='attr',
-                                train_transform=transform,
-                                val_transform=transform,
-                                download=True,
-                                batch_size=bs)
+    dm = CelebADataModule(
+        data_dir=DATA_PATH,
+        target_type="attr",
+        train_transform=transform,
+        val_transform=transform,
+        download=True,
+        batch_size=bs,
+    )
     return dm
 
 
@@ -288,8 +284,9 @@ def main(argv):
 
     dm = celeba_dataloader(bs, IMAGE_SIZE, CROP, DATA_PATH)
 
-    dm.prepare_data() # force download now
-    dm.setup() # force make data loaders 
+    dm.prepare_data()  # force download now
+    dm.setup()  # force make data loaders
 
-if __name__ == '__main__':
-  app.run(main)
+
+if __name__ == "__main__":
+    app.run(main)
