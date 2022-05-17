bceca178e3c6e81b656c92de3ee14a6dc47a10db
karm-patel <karmpatel216@gmail.com>
2022-05-17 23:19:51 +0530

    Handle automated pipeline to update fig in database

--- internal/book2/wrap_with_try_except.py
+++ internal/book2/wrap_with_try_except.py
@@ -3,16 +3,11 @@ import sys
 
 sys.path.append("misc")
 
-from wrap_try_accept import apply_fun_to_notebook, remove_superimport, wrap_try_accept_in_code, remove_pyprobml
+from wrap_try_accept import apply_fun_to_notebook, remove_superimport, wrap_try_accept_in_code, replace_pyprobml_utils
 
 book2_nb = glob("notebooks/book2/*/*.ipynb")
 
-counter = 0
-
 for nb in book2_nb:
-    apply_fun_to_notebook(nb, remove_superimport)
-    apply_fun_to_notebook(nb, wrap_try_accept_in_code)
-    apply_fun_to_notebook(nb, remove_pyprobml)
-    counter += 1
-
-print("Done. {} notebooks are wrapped.".format(counter))
+    # apply_fun_to_notebook(nb, remove_superimport)
+    # apply_fun_to_notebook(nb, wrap_try_accept_in_code)
+    apply_fun_to_notebook(nb, replace_pyprobml_utils)
