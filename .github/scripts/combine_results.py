8f60153a621543bfa82b7c8d55aa3113839037d9
karm-patel <karmpatel216@gmail.com>
2022-05-17 21:23:45 +0530

    Book2: Fix  workflow broken log links

--- .github/scripts/combine_results.py
+++ .github/scripts/combine_results.py
@@ -1,10 +1,18 @@
 import os
 import re
+import argparse
+
+parser = argparse.ArgumentParser(description="combine results")
+parser.add_argument("-user_name", "--user_name", type=str, help="")
+args = parser.parse_args()
+user, repo = args.user_name.split("/")
 
 # Read number of jobs
 with open(".github/workflows/notebooks.yml") as f:
     runners = re.findall("runner_id: (.*)", f.read())
     n_jobs = len(eval(runners[0]))
+
+print(f"{n_jobs} jobs found")
 
 # Collect status, logs and figures
 FOLDERS = ["workflow_testing_indicator", "auto_generated_figures"]
@@ -13,13 +21,13 @@ for folder in FOLDERS:
         os.system("rm -rf " + folder)
     os.makedirs(folder)
 
-repo = "https://github.com/probml/pyprobml.git"
+repo_url = f"https://github.com/{user}/{repo}.git"
 for job in range(n_jobs):
     for folder in FOLDERS:
         branch = f"{folder}_{job}"
         if os.path.exists(branch):
             os.system(f"rm -rf {branch}")
-        os.system(f"git clone --depth 1 --branch {branch} {repo} {branch}")
+        os.system(f"git clone --depth 1 --branch {branch} {repo_url} {branch}")
         os.system(f"cp -r {branch}/* {folder}")
         print(f"Copied {branch} to {folder}")
         os.system(f"rm -rf {branch}")
