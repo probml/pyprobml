9642facb72a8c086cb4226ee9c5c08a3a91cd4c8
karm-patel <karmpatel216@gmail.com>
2022-05-18 16:58:03 +0530

    Added `-qq` in pip install

--- internal/book2/postprocessing.py
+++ internal/book2/postprocessing.py
@@ -16,10 +16,15 @@ def postprocessing(code):
     code = code.replace("%pip install pgmpy_utils", "%pip install git+https://github.com/probml/probml-utils.git pgmpy")
     return code
 
+def add_pip_qq_flag(code):
+    if "pip install" in code:
+        if "pip install -q" not in code:
+            code = code.replace("pip install", "pip install -qq")
+    return code
+        
 
 if __name__ == "__main__":
-    notebooks = glob("notebooks/book2/*/*.ipynb")
+    notebooks = glob("notebooks/book1/*/*.ipynb") + glob("notebooks/book2/*/*.ipynb")
     for notebook in notebooks:
         print(f"******* {notebook} *******")
-        apply_fun_to_notebook(notebook, postprocessing)
-        # apply_fun_to_notebook(notebook, comment_pip_install)
+        apply_fun_to_notebook(notebook, add_pip_qq_flag)
