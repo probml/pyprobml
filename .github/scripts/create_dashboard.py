0d7b5bce7e3eb7f4e0025694ddbdbb8d4d3ac4fc
karm-patel <karmpatel216@gmail.com>
2022-05-19 09:32:19 +0530

    Fix bug in `create_dashboard.py`

--- .github/scripts/create_dashboard.py
+++ .github/scripts/create_dashboard.py
@@ -5,15 +5,10 @@ import argparse
 parser = argparse.ArgumentParser(description="create dashboard")
 parser.add_argument("-user_name", "--user_name", type=str, help="")
 args = parser.parse_args()
-
-
-
-
 user, repo = args.user_name.split("/") # github.repository gives owner/repo
+
 print(f"\n**** creating dashboard on {user}/{repo} *********\n")
-
 base_url = f"https://github.com/{user}/{repo}/tree/"
-
 get_url = lambda x: f'<img width="20" alt="image" src=https://raw.githubusercontent.com/{user}/{repo}/{x}>'
 get_nb_url = lambda x: os.path.join(base_url, "master", x.split("/", 1)[-1].replace(".png", ".ipynb"))
 
@@ -23,10 +18,11 @@ def sort_key(x):
     return (parts[-3], parts[-2])
 
 # write an md file
-log_counter = 0
-file_counter = 0
 with open("workflow_testing_indicator/README.md", "w") as f:
     for book_no in [1,2]:
+        log_counter = 0
+        file_counter = 0
+
         statuses = glob(f"workflow_testing_indicator/notebooks/book{book_no}/*/*.png")
         print(f"**** {len(statuses)} statuses found in Book{book_no}****")
         statuses = sorted(statuses, key=sort_key)
@@ -54,6 +50,6 @@ with open("workflow_testing_indicator/README.md", "w") as f:
         f.write(f"\n")
         f.write(f"## Summary\n")
         f.write(f"\n")
-        final_log = f"In total, {file_counter} jobs were tested.\n{log_counter} jobs failed.\n"
+        final_log = f"Book{book_no}: In total, {file_counter} jobs were tested.\n{log_counter} jobs failed.\n"
         f.write(final_log)
         print(final_log)
