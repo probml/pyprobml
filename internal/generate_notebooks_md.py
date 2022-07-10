928c310e20683bcd1a99a9fa2cc1d0de9f891dd2
karm-patel <karmpatel216@gmail.com>
2022-07-10 23:16:54 +0530

    dynamic updation of `notebooks.md` & `fig_x_y` notebooks removed

--- internal/generate_notebooks_md.py
+++ internal/generate_notebooks_md.py
@@ -2,6 +2,7 @@ from glob import glob
 import pandas as pd
 import os
 import nbformat
+import numpy as np
 import probml_utils.url_utils as url_utils
 
 root_path = "notebooks"
@@ -38,7 +39,7 @@ def get_original_nb(df_nb_list_grp_ser):
     from duplicate notebooks, identify notebook which does not
     have "Source of this notebook" keyword, which is original nb
     """
-    nb_name = df_nb_list_grp_ser["nb_name"]
+    nb_name = df_nb_list_grp_ser["Notebook"]
     books = df_nb_list_grp_ser["book_no"]
     chaps = df_nb_list_grp_ser["chap_no"]
     t = []
@@ -57,7 +58,7 @@ def get_root_col(df_root_ser, col):
            OUTPUT: 11
     """
     is_source = df_root_ser["is_source_present"]
-    nb_name = df_root_ser["nb_name"]
+    nb_name = df_root_ser["Notebook"]
 
     if is_source.count(0) == 0:
         print(f"0: Not in pyprobml!:  {nb_name}")
@@ -70,82 +71,89 @@ def get_root_col(df_root_ser, col):
         return df_root_ser[col][is_source.index(0)]
 
 
-# handle book1
+def is_github_notebook(url):
+    if url.startswith("https://github.com") and url.endswith(".ipynb"):
+        return True
+    return False
+
+
+to_md_url = lambda text, url: f"[{text}]({url})"  # make md url
+
+
+def to_colab_md_url(github_url):
+    if is_github_notebook(github_url):
+        colab_url = url_utils.github_url_to_colab_url(github_url)
+        return to_md_url("colab", colab_url)
+    else:
+        return "NA"
+
+
+# handle pyprobml's book1 & book2 notebooks
 book1_notebooks = glob("notebooks/book1/*/*.ipynb") + glob("notebooks/book2/*/*.ipynb")
 nb_list = list(map(split_nb_path, book1_notebooks))
 
 # make dataframe of notebook
-df_nb_list = pd.DataFrame(nb_list, columns=["book_no", "chap_no", "nb_name"])
+df_pyprobml = pd.DataFrame(nb_list, columns=["book_no", "chap_no", "Notebook"])
 
 # group by duplicate notebooks
-df_nb_list_grp = df_nb_list.groupby("nb_name").agg(lambda x: list(x)).reset_index()
+df_pyprobml = df_pyprobml.groupby("Notebook").agg(lambda x: list(x)).reset_index()
 
 # check if "Source of this notebook" is present
-df_nb_list_grp["is_source_present"] = df_nb_list_grp.apply(get_original_nb, axis=1)
+df_pyprobml["is_source_present"] = df_pyprobml.apply(get_original_nb, axis=1)
 
 # get root notebook
-df_root = df_nb_list_grp
-df_root["chap_no"] = df_nb_list_grp.apply(get_root_col, col="chap_no", axis=1)
-df_root["book_no"] = df_nb_list_grp.apply(get_root_col, col="book_no", axis=1)
-
-
-# Add colab url
-df_root["colab_url"] = df_root.apply(
-    lambda x: url_utils.make_url_from_chapter_no_and_script_name(
-        chapter_no=int(x["chap_no"]),
-        script_name=x["nb_name"],
-        book_no=int(x["book_no"][-1]),
-        convert_to_which_url="colab",
-    ),
-    axis=1,
-)
+df_pyprobml["chap_no"] = df_pyprobml.apply(get_root_col, col="chap_no", axis=1)
+df_pyprobml["book_no"] = df_pyprobml.apply(get_root_col, col="book_no", axis=1)
 
 # Add github url
-df_root["github_url"] = df_root.apply(
+df_pyprobml["type"] = "github"
+df_pyprobml["github_url"] = df_pyprobml.apply(
     lambda x: url_utils.make_url_from_chapter_no_and_script_name(
         chapter_no=int(x["chap_no"]),
-        script_name=x["nb_name"],
+        script_name=x["Notebook"],
         book_no=int(x["book_no"][-1]),
         convert_to_which_url="github",
     ),
     axis=1,
 )
 
-enclose_span = lambda text, nb_id: f"<span id={nb_id}>{text}</span>"
-to_md_url = lambda text, url: f"[{text}]({url})"
-
-df_root["md_colab_url"] = df_root.apply(
-    lambda x: to_md_url(enclose_span("colab", x["nb_name"]), x["colab_url"]), axis=1
-)
-df_root["md_github_url"] = df_root.apply(
-    lambda x: to_md_url(enclose_span("github", x["nb_name"]), x["github_url"]), axis=1
-)
-
-df_final = df_root[["nb_name", "md_colab_url", "md_github_url"]]
-df_final.columns = ["Notebook", "Colab url", "Github url"]
+df_pyprobml = df_pyprobml[["Notebook", "type", "github_url"]]
 
 
 # handle supplementary notebooks
 supp_book = glob("notebooks/book2/*/*/*.ipynb") + glob("notebooks/book1/*/*/*.ipynb")
 print(f"{len(supp_book)} supplementary notebooks found")
 
-# convert to github & colab url
+# convert to github url
 github_root = "https://github.com/probml/pyprobml/blob/master/"
-colab_root = "https://colab.research.google.com/github/probml/pyprobml/blob/master/"
-nb_github_colab_list = list(map(lambda x: [x.split("/")[-1], colab_root + x, github_root + x], supp_book))
+nb_github_colab_list = list(map(lambda x: [x.split("/")[-1], "github", github_root + x], supp_book))
+df_supp = pd.DataFrame(nb_github_colab_list, columns=df_pyprobml.columns)
 
-df_supp = pd.DataFrame(nb_github_colab_list, columns=df_final.columns)
 
-# enclose id in <span>
-df_supp["Colab url"] = df_supp.apply(lambda x: to_md_url(enclose_span("colab", x["Notebook"]), x["Colab url"]), axis=1)
-df_supp["Github url"] = df_supp.apply(
-    lambda x: to_md_url(enclose_span("github", x["Notebook"]), x["Github url"]), axis=1
+# handle external notebooks
+df_external = pd.read_csv("internal/external_links.csv")
+common_notebooks = np.intersect1d(df_pyprobml["Notebook"].values, df_external["Notebook"].values)
+
+# drop common notebooks from df_pyprobml and replace with df_external's notebooks
+print(f"Before: {len(df_pyprobml)} pyprobml notebooks found")
+df_pyprobml = df_pyprobml[~df_pyprobml["Notebook"].isin(common_notebooks)]
+print(f"After: {len(df_pyprobml)} pyprobml notebooks found")
+print(f"{len(df_external)} external reference found")
+
+
+# combine all type of notebooks
+df_all = pd.concat([df_pyprobml, df_supp, df_external])
+df_all = df_all.sort_values(by="Notebook", key=lambda col: col.str.lower())
+
+# get colab url from github url
+df_all["colab_url"] = df_all["github_url"].apply(to_colab_md_url)
+
+# enclose in span tag to give id as a Notebook
+enclose_span = lambda text, nb_id: f"<span id={nb_id}>{text}</span>"
+df_all["github_url"] = df_all.apply(
+    lambda x: to_md_url(enclose_span(x["type"], x["Notebook"]), x["github_url"]), axis=1
 )
 
-
-# combine df_final and df_supp
-df_chap_supp = pd.concat([df_final, df_supp])
-df_chap_supp = df_chap_supp.sort_values(by="Notebook", key=lambda col: col.str.lower())
-
 # save to .md file
-df_chap_supp.to_markdown("notebooks.md", index=False)
+df_all = df_all[["Notebook", "github_url", "colab_url"]]
+df_all.to_markdown("notebooks.md", index=False)
